6e59e1daeb645cb84aa906f88b1971c24fbff038
Jim Posen <jim.posen@gmail.com>
2012-11-30 03:10:54 -0500

    Made first taxonomy tests pass.

--- lib/taxonomy.rb
+++ lib/taxonomy.rb
@@ -1,16 +1,33 @@
 class Taxonomy
 
   def initialize(taxonomy)
-    #taxonomy = taxonomy.map |section| { section.downcase + '/' }.join
-    @taxonomy = taxonomy
+    if taxonomy.is_a? String
+      taxonomy = taxonomy.split '/'
+    end
+    @node = find_taxonomy_node(taxonomy)
   end
 
   def to_s
-    @taxonomy.map { |section| section.downcase + '/' }.join
+    @node[:taxonomy].map { |section| section.downcase + '/' }.join
   end
 
   def to_a
-    @taxonomy
+    @node[:taxonomy]
+  end
+
+  private
+
+  def find_taxonomy_node(taxonomy)
+    root = {children: Settings.taxonomy}
+    fullTaxonomy = []
+    taxonomy.each do |section|
+      root = (root[:children] || []).select do |child|
+        child.name.downcase == section.downcase
+      end.first
+      return nil if root.nil?
+      fullTaxonomy << root.name
+    end
+    {taxonomy: fullTaxonomy, children: root.children}
   end
 
 end
