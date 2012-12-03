59f74c592ab643ca4865056cab234936b88660b0
Jim Posen <jim.posen@gmail.com>
2012-12-03 14:09:16 -0500

    Implemented article create action with some tests.

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -4,4 +4,13 @@ class Admin::ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(params[:article])
+    if @article.save
+      redirect_to admin_root_path
+    else
+      render 'new'
+    end
+  end
 end
--- spec/requests/admin/articles_spec.rb
+++ spec/requests/admin/articles_spec.rb
@@ -10,22 +10,38 @@ describe "Admin::Articles" do
       visit new_admin_article_url(:subdomain => :admin)
     end
 
+    let(:submit) { "Create Article" }
+
     describe "with invalid information" do
+      before { fill_in "Subtitle", with: "Oak arrives just in time" }
+
       it "should not create an article" do
-        expect { click_button "Submit" }.not_to change(Article, :count)
+        expect { click_button :submit }.not_to change(Article, :count)
+      end
+
+      it "should display errors" do
+        click_button :submit
+        find_field("Title").native.next_sibling
+          .text.should include("can't be blank")
+        find_field("Title").native.parent.parent
+          .get_attribute(:class).should include('error')
+        find_field("Body").native.next_sibling
+          .text.should include("can't be blank")
+        find_field("Body").native.parent.parent
+          .get_attribute(:class).should include('error')
       end
     end
 
     describe "with valid information" do
       before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Title", with: "Ash defeats Gary in Indigo Plateau"
+        fill_in "Subtitle", with: "Oak arrives just in time"
+        fill_in "Teaser", with: "Ash becomes new Pokemon Champion."
+        fill_in "Body", with: "**Pikachu** wrecks everyone. The End."
       end
 
       it "should create an Article" do
-        expect { click_button "Submit" }.to change(Article, :count).by(1)
+        expect { click_button :submit }.to change(Article, :count).by(1)
       end
     end
   end
