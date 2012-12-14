f1a72fec1e4497f5f7cde149f0241e13372f16ae
Jim Posen <jim.posen@gmail.com>
2012-12-14 16:24:05 -0500

    Wrote Author::find_or_create_all_by_name class method to assist article creation.

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -6,4 +6,19 @@ end
 
 class Author < Staff
   has_and_belongs_to_many :articles
+
+  def self.find_or_create_all_by_name(names)
+    authors = {}
+    Author.where(name: names).each do |author|
+      authors[author.name] = author
+    end
+    names.each do |name|
+      Author.transaction do
+        if not authors.has_key?(name)
+          authors[name] = Author.create(name: name)
+        end
+      end
+    end
+    authors.values
+  end
 end
--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Staff do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Author do
+    describe "::find_or_create_all_by_name" do
+      before(:each) do
+        @author = Author.create(name: 'Ash Ketchum')
+      end
+
+      it "should return authors if they exist" do
+        authors = Author.find_or_create_all_by_name(['Ash Ketchum'])
+        authors.should == [@author]
+      end
+
+      it "should create authors if they don't exist" do
+        expect { Author.find_or_create_all_by_name(['Hiker Martin']) }
+          .to change(Author, :count).by(1)
+      end
+
+      it "should return new authors when they don't exist" do
+        Author.find_or_create_all_by_name(['Ash Ketchum', 'Hiker Martin'])
+          .should == [@author, Author.find_by_name('Hiker Martin')]
+      end
+    end
+  end
 end
