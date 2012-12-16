9c0dceef51daaebd545fb259c8628cc482cc0e6f
Jim Posen <jim.posen@gmail.com>
2012-12-16 18:44:32 -0500

    Added byline to article page.

--- app/helpers/site/article_helper.rb
+++ app/helpers/site/article_helper.rb
@@ -1,2 +1,9 @@
 module Site::ArticleHelper
+
+  def byline(article)
+    article.authors.map do |author|
+      link_to author.name, "hi there"  # TODO: use author url helper
+    end.join(', ').html_safe
+  end
+
 end
