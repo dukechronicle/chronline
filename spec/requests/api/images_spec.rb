28e0ca31e9378bcd144fe18c5a4724cddcbfdc2f
Jim Posen <jim.posen@gmail.com>
2012-12-22 12:34:05 -0500

    Wrote failing test for images API sending url method.

--- spec/requests/api/images_spec.rb
+++ spec/requests/api/images_spec.rb
@@ -5,16 +5,30 @@ describe "Images API" do
   describe "GET /images" do
     before do
       stub_request(:put, /#{Settings.aws.bucket}\.s3\.amazonaws\.com/)
-      @images = FactoryGirl.create_list(:image, 10)
+      @image = FactoryGirl.create(:image)
       get api_images_url(subdomain: :api)
     end
 
     let(:success) { 200 }
-
-    subject { JSON.parse(response.body) }
+    let(:images) { JSON.parse(response.body) }
 
     it { response.status.should be(success) }
-    it { should be_an(Array) }
-    it { should have(10).images }
+    it { images.should be_an(Array) }
+    it { images.should have(1).images }
+
+    it "should have image properties" do
+      attrs = @image.attributes
+      # Timestamps are in different format for JSON
+      attrs.each do |key, value|
+        if value.respond_to?(:iso8601)
+          attrs[key] = value.iso8601
+        end
+      end
+      images.first.should include(attrs)
+    end
+
+    it "should have original url" do
+      images.first.should include(url: @image.original.url)
+    end
   end
 end
