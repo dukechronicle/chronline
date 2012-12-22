937502e013dcf1c096bd1ceb8bbe50ba891e4f21
Jim Posen <jim.posen@gmail.com>
2012-12-21 21:07:54 -0500

    Tested api images controller using FactoryGirl.

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,6 +1,6 @@
 class Api::ImagesController < ApplicationController
   def index
-    images = Image.page(params[:page]).order('created_at DESC')
+    images = Image.order('created_at DESC')
     render json: images
   end
 end
--- spec/requests/api/images_spec.rb
+++ spec/requests/api/images_spec.rb
@@ -3,15 +3,17 @@ require 'spec_helper'
 describe "Images API" do
 
   describe "GET /images" do
-    before { get api_images_url(subdomain: :api) }
+    before do
+      stub_request(:put, /#{Settings.aws.bucket}\.s3\.amazonaws\.com/)
+      @images = FactoryGirl.create_list(:image, 10)
+      get api_images_url(subdomain: :api)
+    end
 
     let(:success) { 200 }
-    let(:images) { FactoryGirl.create_list(:image, 10) }
 
     subject { JSON.parse(response.body) }
 
     it { response.status.should be(success) }
-
     it { should be_an(Array) }
     it { should have(10).images }
   end
