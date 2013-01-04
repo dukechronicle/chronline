6df80c369eb75ceeeec2cb915f35fb91e021eb02
Jim Posen <jim.posen@gmail.com>
2013-01-03 23:46:46 -0500

    Wrote before_filter for Site::BaseController to redirect mobile requests to mobile site.

--- app/controllers/site/base_controller.rb
+++ app/controllers/site/base_controller.rb
@@ -1,3 +1,15 @@
 class Site::BaseController < ApplicationController
   layout 'site'
+
+  before_filter :redirect_mobile
+
+
+  def redirect_mobile
+    if browser.mobile?
+      url = URI(request.url)
+      url.host.sub!(/^www/, 'm')
+      redirect_to url.to_s
+    end
+  end
+
 end
