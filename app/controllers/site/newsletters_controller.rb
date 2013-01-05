b0705318110ef4448e4560280e1408b513605fc1
Jim Posen <jim.posen@gmail.com>
2013-01-05 00:28:59 -0500

    Finished site newsletter controller which subscribes and unsubscribes users.

--- app/controllers/site/newsletters_controller.rb
+++ app/controllers/site/newsletters_controller.rb
@@ -5,4 +5,22 @@ class Site::NewslettersController < Site::BaseController
   def show
   end
 
+  def subscribe
+    @email = params[:email]
+    gb = Gibbon.new(Settings.mailchimp.api_key)
+    gb.list_subscribe(id: Settings.mailchimp.list_id,
+                      email_address: @email,
+                      send_welcome: true)
+    render 'show'
+  end
+
+  def unsubscribe
+    @email = params[:email]
+    gb = Gibbon.new(Settings.mailchimp.api_key)
+    gb.list_subscribe(id: Settings.mailchimp.list_id,
+                      email_address: @email,
+                      send_goodbye: true)
+    render 'show'
+  end
+
 end
