b27239ce21e7479609530cb24103332297ad2e07
Jim Posen <jim.posen@gmail.com>
2013-01-10 14:31:26 -0500

    Created HAML for page form but no Javascript.

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,11 +1,11 @@
 class Admin::PagesController < Admin::BaseController
+
   def index
     @pages = Page.page(params[:page]).order('updated_at DESC')
   end
 
-  def edit
+  def new
+    @page = Page.new
   end
 
-  def new
-  end
 end
