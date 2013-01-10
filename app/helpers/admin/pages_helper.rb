027061841fed88f686775450202305a301bdd0cc
Jim Posen <jim.posen@gmail.com>
2013-01-10 18:28:38 -0500

    Template layout select has data-schema property.

--- app/helpers/admin/pages_helper.rb
+++ app/helpers/admin/pages_helper.rb
@@ -4,4 +4,10 @@ module Admin::PagesHelper
     URI.join("http://www.#{current_domain}", page.path).to_s
   end
 
+  def page_layout_options
+    Page::Layouts.all.each_pair.map do |name, layout_class|
+      [name, name, {'data-schema' => layout_class.new(nil).json_schema.to_json}]
+    end
+  end
+
 end
