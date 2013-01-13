952d225ec4294c3005b21646143b5b5102457ebd
Jim Posen <jim.posen@gmail.com>
2013-01-13 05:23:13 -0500

    Finished news page.

--- lib/layout.rb
+++ lib/layout.rb
@@ -76,9 +76,11 @@ Layout.add_schema(:disqus_popular, {"type" => "null"}) do |invocations|
 end
 
 
-Layout.add_schema(:popular, {"type" => "string"}) do |invocations|
-  disqus = Disqus.new(Settings.disqus.api_key)
-  article_slugs = disqus.popular_articles(Settings.disqus.shortname, 7)
-  articles = Article.includes(:authors, :image).find_in_order(article_slugs)
-  [articles] * invocations.length
+Layout.add_schema(:popular, {
+                    'type' => 'string',
+                    'enum' => Taxonomy.main_sections.map {|t| t.name.downcase},
+                  }) do |sections|
+  sections.map do |section|
+    Article.popular(section, limit: 7)
+  end
 end
