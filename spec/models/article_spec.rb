cb4b110a6e8b04a8eb4c3a84b14584b7452509a8
Faraz Yashar <faraz.yashar@gmail.com>
2013-01-12 23:50:10 -0500

    Update article specs with shoulda matchers

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -31,16 +31,9 @@ describe Article do
   subject { @article }
 
   it { should be_valid }
-
-  describe "when body is not present" do
-    before { @article.body = "" }
-    it { should_not be_valid }
-  end
-
-  describe "when title is not present" do
-    before { @article.title = "" }
-    it { should_not be_valid }
-  end
+  it { should have_and_belong_to_many :authors }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
 
   describe "#section" do
     it { @article.section.should be_a_kind_of(Taxonomy) }
