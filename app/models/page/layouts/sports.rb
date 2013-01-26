4239026d6069420841e15dfd5d78245510ffb471
Jim Posen <jim.posen@gmail.com>
2013-01-26 17:19:14 -0500

    Created sports page.

---
+++ app/models/page/layouts/sports.rb
@@ -0,0 +1,34 @@
+class Page::Layouts::Sports < Layout
+
+  def schema
+    {
+      'featured' => {
+        'label' => "Slideshow",
+        'type' => 'array',
+        'required' => true,
+        'items' => article_schema,
+      },
+      'blog' => {
+        'label' => 'Blog RSS Feed',
+        'extends' => rss_schema,
+        'required' => true,
+      },
+      'multimedia' => {
+        'label' => 'Embedded Multimedia',
+        'type' => 'array',
+        'required' => true,
+        'items' => {
+          'type' => 'string',
+          'format' => 'multiline',
+        }
+      },
+      'bottom_articles' => {
+        'label' => 'Bottom Articles',
+        'type' => 'array',
+        'required' => true,
+        'items' => article_schema,
+      },
+    }
+  end
+
+end
