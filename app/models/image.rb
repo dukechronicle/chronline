05e663bc6509de23e88956dc7239158f867aa352
Michael Lai <themichaellai@gmail.com>
2013-02-02 16:38:18 -0500

    Added credit column to images

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,6 +5,7 @@
 #  id                    :integer          not null, primary key
 #  caption               :string(255)
 #  location              :string(255)
+#  credit                :string(255)
 #  original_file_name    :string(255)
 #  original_content_type :string(255)
 #  original_file_size    :integer
@@ -29,7 +30,7 @@ class Image < ActiveRecord::Base
     end.to_h
   end
 
-  attr_accessible :caption, :date, :location, :original
+  attr_accessible :caption, :date, :location, :original, :credit
   attr_accessor :crop_style, :crop_x, :crop_y, :crop_w, :crop_h
   has_attached_file :original, styles: self.styles, processors: [:cropper]
 
