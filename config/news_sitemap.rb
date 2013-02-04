3937695af5de89032e90121d94f12e0b9d0894ad
Jim Posen <jim.posen@gmail.com>
2013-02-03 19:50:01 -0500

    Added images to news sitemap.

--- config/news_sitemap.rb
+++ config/news_sitemap.rb
@@ -1,21 +1,34 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = Settings.domain
-SitemapGenerator::Sitemap.sitemaps_host = "http://s3.amazonaws.com/"+Settings.aws.bucket
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
+SitemapGenerator::Sitemap.default_host = "http://www.#{Settings.domain}"
+SitemapGenerator::Sitemap.sitemaps_host = "http://#{Settings.aws.bucket}.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.public_path = "tmp/"
 SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/news"
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
 
 SitemapGenerator::Sitemap.create do
-  add '/articles', :changefreq => 'daily', :news => {
-    :publication_name => "Duke Chronicle",
-    :publication_language => "en"
-  }
-
-  Article.where(["created_at >= ?", 2.days.ago]) do |article|
-    add site_article_path(article), :lastmod => article.updated_at, :news => {
-      :publication_name => "Duke Chronicle",
-      :publication_language => "en",
-      :title => article.title,
-      :publication_date => article.created_at
-    }
+  Article.all do |article| #where(["created_at >= ?", 2.days.ago]) do |article|
+    p article.title
+    add(site_article_path(article),
+        lastmod: article.updated_at,
+        news: news_information(article),
+        image: image_information(article),
+        )
   end
+end
+
+def news_information(article)
+  {
+    publication_name: "Duke Chronicle",
+    publication_language: "en",
+    title: article.title,
+    publication_date: article.created_at
+  }
+end
+
+def image_information(article)
+  return [] unless article.image
+  [{
+     loc: article.image.original.url(:large_rect),
+     caption: article.caption,
+  }]
 end
