3b5113537d5933497e60ad5aff9d617cc1f1c7ca
Jim Posen <jim.posen@gmail.com>
2013-02-05 05:02:26 -0500

    Set initialize_on_precompile to false so that compilation works on Heroku.

--- config/application.rb
+++ config/application.rb
@@ -79,6 +79,7 @@ module Chronline
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
 
     config.action_mailer.default_url_options = {host: Settings.domain}
 
