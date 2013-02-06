18f35a08bdfe2919632544a61437812b053c5107
Jim Posen <jim.posen@gmail.com>
2013-02-05 22:40:05 -0500

    Modified page form so that a description and image can be added.

--- app/models/image.rb
+++ app/models/image.rb
@@ -42,6 +42,7 @@ class Image < ActiveRecord::Base
   validates :date, presence: true
 
   has_many :articles
+  has_many :pages
   has_many :staff, foreign_key: :headshot_id
   belongs_to :photographer, class_name: "Staff"
 
