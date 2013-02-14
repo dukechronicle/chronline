1c2ee5069aeaea155a77f7e7de07d99949e50572
Jim Posen <jim.posen@gmail.com>
2013-02-14 14:19:32 -0500

    Used a different routing scheme to clean up staff controller.

--- app/controllers/site/staff_controller.rb
+++ app/controllers/site/staff_controller.rb
@@ -1,27 +1,21 @@
 class Site::StaffController < Site::BaseController
 
-  def index
-    if params[:view] == :images
-      @staff = Staff.find(params[:staff_id])
-      @images = @staff.images.page(params[:page]).order('created_at DESC')
-      @except = params[:view]
-      render 'site/staff/photographer'
-    else
-      @except = params[:view]
-      @staff = Staff.find(params[:staff_id])
-      @articles = @staff.articles.page(params[:page]).order('created_at DESC')
-      render 'site/staff/author'
-    end
-  end
-  def show
+  def articles
     @staff = Staff.find(params[:id])
     @articles = @staff.articles.page(params[:page]).order('created_at DESC')
-    if @staff.author?
-      redirect_to site_staff_articles_path params[:id]
-    elsif @staff.photographer?
-      redirect_to site_staff_images_path params[:id]
+  end
+
+  def images
+    @staff = Staff.find(params[:id])
+    @images = @staff.images.page(params[:page]).order('date DESC')
+  end
+
+  def show
+    @staff = Staff.find(params[:id])
+    if @staff.photographer? and not @staff.author?
+      redirect_to images_site_staff_path(@staff)
     else
-      redirect_to site_staff_articles_path params[:id]
+      redirect_to articles_site_staff_path(@staff)
     end
   end
 
