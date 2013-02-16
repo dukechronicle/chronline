2846edda6320f6b3e730920591236559899d27f0
Jim Posen <jim.posen@gmail.com>
2013-02-16 17:33:33 -0500

    Index the author names in full text search.

--- app/models/article.rb
+++ app/models/article.rb
@@ -41,6 +41,9 @@ class Article < ActiveRecord::Base
   searchable do
     text :title, stored: true, boost: 2.0, more_like_this: true
     text :body, stored: true, more_like_this: true
+    text :author_names do  # Staff names rarely change
+      authors.map(&:name)  # TODO: show staff records if name is searched
+    end
     integer :author_ids, multiple: true
     string :section do
       section[0]
