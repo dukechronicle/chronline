b2394d18cc784240f711488d5369e937d1259b94
Jim Posen <jim.posen@gmail.com>
2013-02-19 13:05:50 -0500

    Set image Cache-Control header to public.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -7,7 +7,7 @@ if Rails.env.production?
       :secret_access_key => Settings.aws.secret_access_key,
     },
     s3_host_alias: Settings.content_cdn,
-    s3_headers: {'Cache-Control' => 'max-age=315576000'},
+    s3_headers: {'Cache-Control' => 'public,max-age=315576000'},
     url: ':s3_alias_url',
     path: 'images/:style/:hash.:extension',
     hash_secret: 'super secret string',  # TODO: do something better
