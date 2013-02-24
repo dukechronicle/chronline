1c70c188caaedff0d221aad5298ed182d8abf16f
Jim Posen <jim.posen@gmail.com>
2013-02-24 13:51:10 -0500

    Moved some article methods to Postable module.

--- app/models/postable.rb
+++ app/models/postable.rb
@@ -2,9 +2,31 @@ module Postable
 
   def self.included(base)
     base.send(:include, FriendlyId)
-    base.attr_accessible :image_id, :title
+    base.attr_accessible :body, :image_id, :title
     base.friendly_id(:title, use: [:slugged, :history])
     base.belongs_to :image
+
+    base.validates :body, presence: true
+    base.validates :title, presence: true, length: {maximum: 90}
+  end
+
+  # Stolen from http://snipt.net/jpartogi/slugify-javascript/
+  def normalize_friendly_id(title, max_chars=50)
+    return nil if title.nil?  # record won't save -- title presence is validated
+    removelist = %w(a an as at before but by for from is in into like of off on
+onto per since than the this that to up via with)
+    r = /\b(#{removelist.join('|')})\b/i
+
+    s = title.downcase  # convert to lowercase
+    s.gsub!(r, '')
+    s.strip!
+    s.gsub!(/[^-\w\s]/, '')  # remove unneeded chars
+    s.gsub!(/[-\s]+/, '-')   # convert spaces to hyphens
+    s[0...max_chars].chomp('-')
+  end
+
+  def render_body
+    RDiscount.new(body).to_html  # Uses RDiscount markdown renderer
   end
 
 end
