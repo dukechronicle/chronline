122ad13221b5430509e4341815fba82f47527ca3
Michael Lai <themichaellai@gmail.com>
2013-02-24 14:48:59 -0500

    Embeded -> Embedded

--- app/models/article.rb
+++ app/models/article.rb
@@ -89,7 +89,7 @@ onto per since than the this that to up via with)
 
   def render_body
     html = RDiscount.new(body).to_html  # Uses RDiscount markdown renderer
-    EmbededMedia.new(html).to_s
+    EmbeddedMedia.new(html).to_s
   end
 
   def section
