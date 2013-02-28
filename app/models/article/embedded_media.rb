d5c3e7a2fbcb6a7ccbe1a50134a25464d34bb52d
Michael Lai <themichaellai@gmail.com>
2013-02-28 18:38:41 -0500

    Integrated image picker

--- app/models/article/embedded_media.rb
+++ app/models/article/embedded_media.rb
@@ -19,6 +19,7 @@ class Article::EmbeddedMedia
     ids = @body.scan(/{{Image:([0-9]*)}} ?/).to_set.to_a
     unless ids.empty?
       images_by_id = Hash[ Image.find_all_by_id(ids).map{ |img| [img.id, img] } ]
+      # chomps space after tag
       @body.gsub!(/{{Image:([0-9]*)}} ?/) do |id|
         if images_by_id.has_key? $1.to_i
           "<img src=\"#{images_by_id[$1.to_i].original.url(:thumb_rect)}\" />"
