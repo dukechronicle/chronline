091da199a6aa62ed1655e3fc2417d5c7de365527
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-03-03 17:47:13 -0500

    added bracket json schema

--- app/models/page/layouts/bracket.rb
+++ app/models/page/layouts/bracket.rb
@@ -1,35 +1,56 @@
 class Page::Layouts::Bracket < Layout
   def schema
    {
-    'description' => 'Bracket Schema',
-    'type' => 'object',
-    'properties' => {
-      'teams' => {
+    'teams' => {
+      'label' => 'Teams',
+      'type' => 'array',
+      'items' => {
+        'type' => 'object',
+        'properties' => {
+          'school' => { 
+            'label' => 'school',
+            'type' => 'string' 
+          },
+          'image'  => { 
+            'label' => 'image',
+            'type' => 'string' 
+          },
+          'seed'   => { 
+            'label' => 'seed',
+            'type' => 'number' 
+          }
+      },
+      'required' => ['school', 'image', 'seed']
+      }
+    },
+    'standings' => {
+      'label' => 'Rounds Standings',
+      'type' => 'array',
+      'items' => {
+        'label' => 'Round',
         'type' => 'array',
         'items' => {
+          'label' => 'Match',
           'type' => 'object',
           'properties' => {
-            'school' => { 'type' => 'string' },
-            'image'  => { 'type' => 'string' },
-            'seed'   => { 'type' => 'number' }
-          },
-          'required' => ['school', 'image', 'seed']
-        }
-      },
-      'standings' => {
-        'type' => 'array',
-        'items' => {
-          'type' => 'array',
-          'items' => {
-            'type' => 'object',
-            'properties' => {
-              'team1'  => { 'type' => 'number' },
-              'team2'  => { 'type' => 'number' },
-              'score1' => { 'type' => 'number' },
-              'score2' => { 'type' => 'number' }
+            'team1'  => { 
+              'label' => 'team1',
+              'type' => 'number' 
             },
-            'required' => ['team1', 'team2']
-          }
+            'team2'  => { 
+              'label' => 'team2',
+              'type' => 'number' 
+            },
+            'score1' => { 
+              'label' => 'score1',
+              'type' => 'number' 
+            },
+            'score2' => { 
+              'label' => 'score2',
+              'type' => 'number' 
+            }
+          },
+          'required' => ['team1', 'team2']
         }
       }
     }
