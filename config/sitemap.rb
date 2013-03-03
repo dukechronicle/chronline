5aa86b6fd19f26c44e0a3087491465c2904d8ebc
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-03-03 13:30:13 -0500

    added actual full sitemap functionality

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,9 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.dukechronicle.com"
+SitemapGenerator::Sitemap.default_host = "http://www.#{Settings.domain}"
+SitemapGenerator::Sitemap.sitemaps_host = "http://#{Settings.aws.bucket}.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.public_path = "tmp/"
+SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/full"
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
 
 SitemapGenerator::Sitemap.create do
   add '/articles', :changefreq => 'daily'
