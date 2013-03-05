c1b2915f2c78d6ef67f56e4069ad0ddcb066c018
Jim Posen <jim.posen@gmail.com>
2013-03-04 19:28:44 -0500

    Article pages don't redirect for Facebook crawler.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  CRAWLERS = [/facebookexternalhit\//i]
+
   protect_from_forgery
   cache_sweeper :article_sweeper
+
+
+  protected
+
+  def social_crawler?
+    CRAWLERS.any? {|crawler| request.user_agent =~ crawler}
+  end
+
 end
