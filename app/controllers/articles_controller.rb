0d502b910d6f8a2a9ed64cbb6aaa699fc6796b40
Jim Posen <jim.posen@gmail.com>
2013-03-04 19:28:44 -0500

    Article pages don't redirect for Facebook crawler.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ module ArticlesController
       .find(params[:id])
     expected_path = url_for(controller: controller_path, action: action_name,
                             id: @article, only_path: true)
-    if request.path != expected_path
+    if not social_crawler? and request.path != expected_path
       return redirect_to expected_path, status: :moved_permanently
     end
   end
