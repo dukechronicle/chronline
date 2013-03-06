72cca503c276171f5af00215d7903c8817a05cf6
Jim Posen <jim.posen@gmail.com>
2013-03-06 00:36:56 -0500

    Serialized blog object as the blog id.

--- app/models/blog/post.rb
+++ app/models/blog/post.rb
@@ -1,10 +1,21 @@
 class Blog
+  class Encoder
+    def load(blog_id)
+      Blog.find(blog_id)
+    end
+
+    def dump(blog)
+      blog.id
+    end
+  end
+
   class Post < ActiveRecord::Base
     include Postable
 
     self.table_name = :blog_posts
 
     attr_accessible :blog
+    serialize :blog, Blog::Encoder.new
 
     validates :blog, presence: true
   end
