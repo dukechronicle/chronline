8cfb929e052e1d553280dbde5bb0b37d18541f5c
Jim Posen <jim.posen@gmail.com>
2013-03-17 18:07:38 -0400

    Fixed the Postable module such that it correctly overrides normalize_friendly_id.

--- app/models/postable.rb
+++ app/models/postable.rb
@@ -2,33 +2,41 @@ module Postable
 
   def self.included(base)
     base.send(:include, FriendlyId)
-    base.attr_accessible :body, :image_id, :title
     base.friendly_id(:title, use: [:slugged, :history])
+
+    base.send(:include, InstanceMethods)
+    base.attr_accessible :body, :image_id, :title
     base.belongs_to :image
 
     base.validates :body, presence: true
     base.validates :title, presence: true, length: {maximum: 90}
   end
 
-  # Stolen from http://snipt.net/jpartogi/slugify-javascript/
-  def normalize_friendly_id(title, max_chars=100)
-    return nil if title.nil?  # record won't save -- title presence is validated
-    removelist = %w(a an as at before but by for from is in into like of off on
-onto per since than the this that to up via with)
-    r = /\b(#{removelist.join('|')})\b/i
-
-    s = title.downcase  # convert to lowercase
-    s.gsub!(r, '')
-    s.strip!
-    s.gsub!(/[^-\w\s]/, '')  # remove unneeded chars
-    s.gsub!(/[-\s]+/, '-')   # convert spaces to hyphens
-    s[0...max_chars].chomp('-')
-
-    (created_at || Date.today).strftime('%Y/%m/%d/') + s
-  end
-
   def render_body
     RDiscount.new(body).to_html  # Uses RDiscount markdown renderer
+  end
+
+  # Can't define them directly, as this must be included after FriendlyId
+  # so that normalize_friendly_id is not overridden
+  module InstanceMethods
+
+    # Stolen from http://snipt.net/jpartogi/slugify-javascript/
+    def normalize_friendly_id(title, max_chars=100)
+      return nil if title.nil?  # record won't save -- title presence is validated
+      removelist = %w(a an as at before but by for from is in into like of off on
+onto per since than the this that to up via with)
+      r = /\b(#{removelist.join('|')})\b/i
+
+      s = title.downcase  # convert to lowercase
+      s.gsub!(r, '')
+      s.strip!
+      s.gsub!(/[^-\w\s]/, '')  # remove unneeded chars
+      s.gsub!(/[-\s]+/, '-')   # convert spaces to hyphens
+      s[0...max_chars].chomp('-')
+
+      (created_at || Date.today).strftime('%Y/%m/%d/') + s
+    end
+
   end
 
 end
