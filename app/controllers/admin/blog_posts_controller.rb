3bce8d87eea324f2a400229b0f40f83dfd7a2a60
Jim Posen <jim.posen@gmail.com>
2013-03-17 18:33:44 -0400

    Implemented all admin controller actions for blog posts.

--- app/controllers/admin/blog_posts_controller.rb
+++ app/controllers/admin/blog_posts_controller.rb
@@ -14,13 +14,30 @@ class Admin::BlogPostsController < Admin::BaseController
   def create
     @blog_post = Blog::Post.new(params[:blog_post])
     if @blog_post.save
-      redirect_to admin_blogs_path
+      admin_blog_posts_path(params[:blog_id])
     else
       render 'new'
     end
   end
 
   def edit
+    @blog_post = Blog::Post.find(params[:id])
+  end
+
+  def update
+    @blog_post = Blog::Post.find(params[:id])
+    if @blog_post.update_attributes(params[:blog_post])
+      admin_blog_posts_path(params[:blog_id])
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    blog_post = Blog::Post.find(params[:id])
+    blog_post.destroy
+    flash[:success] = %Q[Blog post "#{blog_post.title}" was deleted.]
+    redirect_to admin_blog_posts_path(params[:blog_id])
   end
 
 end
