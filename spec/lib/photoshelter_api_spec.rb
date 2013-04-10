d6b0a2fd7533ed53864ba1371531ce0d759c3324
Prithvi Prabahar <prithvi923@gmail.com>
2013-04-10 15:49:02 -0400

    Add pending tests to spec

--- spec/lib/photoshelter_api_spec.rb
+++ spec/lib/photoshelter_api_spec.rb
@@ -1,26 +1,35 @@
 require 'spec_helper'
 
 describe PhotoshelterAPI do
-  subject { PhotoshelterAPI.new(Settings['photoshelter']['email'], Settings['photoshelter']['password']) }
+  subject { PhotoshelterAPI.new(Settings.photoshelter.email, Settings.photoshelter.password) }
 
   context "when constructed with an invalid email" do
     it "should raise an error" do
-      constructor = lambda { PhotoshelterAPI.new("fail", Settings['photoshelter']['password']) }
+      constructor = -> { PhotoshelterAPI.new("fail", Settings.photoshelter.password) }
       constructor.should raise_error(StandardError)
     end
   end
 
   context "when constructed with an invalid password" do
     it "should raise an error" do
-      constructor = lambda { PhotoshelterAPI.new(Settings['photoshelter']['email'], "fail") }
+      constructor = -> { PhotoshelterAPI.new(Settings.photoshelter.email, "fail") }
       constructor.should raise_error(StandardError)
     end
   end
 
-# To test
-# galleries = api.get_all_galleries
-# images = api.get_gallery_images galleries.first["id"]
-# puts api.get_image_info images.first["id"]
-# api.logout
+  describe "#get_all_galleries" do
+    it "should return list of galleries"
+  end
 
+  describe "#get_gallery_images" do
+    it "should return a list of images for a gallery"
+  end
+
+  describe "#get_image_info" do
+    it "should return information for an image"
+  end
+
+  describe "#logout" do
+    it "should logout and destroy the session"
+  end
 end
