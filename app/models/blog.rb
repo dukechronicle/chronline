7bde41059e3fb55cd25e88ef82b4b1912515260a
Jim Posen <jim.posen@gmail.com>
2013-04-11 18:16:53 -0400

    Added images for blog boxes.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -11,7 +11,7 @@ class Blog
   # Blog class is not publicly instantiable
   private_class_method :new
 
-  attr_accessor :id, :name
+  attr_accessor :id, :name, :image
 
 
   def initialize(attributes={})
