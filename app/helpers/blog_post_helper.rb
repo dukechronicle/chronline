aac33f38929b105438cf8037989c92faf7c2b504
Jim Posen <jim.posen@gmail.com>
2013-04-14 18:42:15 -0400

    Blog box has recent posts.

--- app/helpers/blog_post_helper.rb
+++ app/helpers/blog_post_helper.rb
@@ -8,4 +8,8 @@ module BlogPostHelper
     Blog.all.map {|blog| [blog.name, blog.id]}
   end
 
+  def recent_blog_posts(blog, limit=5)
+    Blog::Post.where(blog: blog.id).order('created_at DESC').limit(limit)
+  end
+
 end
