453f7ae3eea2e31043839884d2e5b4730ac68c5c
Jim Posen <jim.posen@gmail.com>
2013-04-15 12:26:27 -0400

    Wrote more specs for Blog model.

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
-Blog::Data['pokedex'] = {
-  'name' => 'The Pokedex'
-}
+Blog.const_set(:Data, 'pokedex' => {
+                 'name' => "The Pokedex",
+                 'description' => "Gotta catch em all",
+               })
 
 
 describe Blog do
@@ -11,15 +12,38 @@ describe Blog do
     ->{Blog.new}.should raise_error
   end
 
-  subject { Blog.find('pokedex') }
-
-  # Attributes for 'pokedex' blog are in config/settings/test.yml
-  its(:id) { should == 'pokedex' }
-  its(:name) { should == 'The Pokedex' }
+  let(:blog) { Blog.find('pokedex') }
+  subject { blog }
 
   # Required to comply with ActiveModel interface
   it { should be_persisted }
 
-  it { subject.to_param.should == 'pokedex' }
+  describe "::find" do
+    # Attributes for 'pokedex' blog are in config/settings/test.yml
+    its(:id) { should == 'pokedex' }
+    its(:name) { should == 'The Pokedex' }
+    its(:description) { should == "Gotta catch em all" }
+  end
+
+  describe "#==" do
+    it { should_not == double("NotABlog", id: 'pokedex') }
+    it { should_not == Blog.send(:new, id: 'Johto') }
+    it { should == Blog.send(:new, id: 'pokedex') }
+  end
+
+  describe "#to_param" do
+    it { subject.to_param.should == 'pokedex' }
+  end
+
+  describe "::all" do
+    subject { Blog.all }
+
+    it { should have(1).blog }
+    it { should include(blog) }
+  end
+
+  describe "::each" do
+    it { expect {|b| Blog.each(&b) }.to yield_successive_args(subject) }
+  end
 
 end
