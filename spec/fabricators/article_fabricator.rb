a511c9fefec84e5f31037135b8fc04c3c5e2b2fc
Jim Posen <jim.posen@gmail.com>
2013-05-27 22:07:11 -0700

    Used clean spec_helper.

--- spec/fabricators/article_fabricator.rb
+++
@@ -1,8 +0,0 @@
-Fabricator(:article) do
-  title { Faker::Lorem.words(5).join(' ') }
-  subtitle "Oak arrives just in time"
-  teaser "Ash becomes new Pokemon Champion."
-  body "Pikachu wrecks everyone. The End."
-  section "/news/university"
-  authors(count: 1, fabricator: :staff)
-end
--- spec/fabricators/staff_fabricator.rb
+++
@@ -1,8 +0,0 @@
-Fabricator(:staff) do
-  name { Faker::Name.name }
-  affiliation "PokeTrainer"
-  tagline "Wanna be the very best"
-  twitter "pokefan"
-  columnist false
-  biography "The best Pokemon trainer ever."
-end
