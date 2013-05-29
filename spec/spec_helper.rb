4189667416437c6177334c0655b50152e871b472
Jim Posen <jim.posen@gmail.com>
2013-05-29 00:09:39 -0700

    Added more unit tests for Image model.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
+require 'paperclip/matchers'
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'webmock/rspec'
 require 'sunspot/rails/spec_helper'
+require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -40,4 +41,6 @@ RSpec.configure do |config|
 
   # Formatting
   config.formatter = :documentation
+
+  config.include Paperclip::Shoulda::Matchers
 end
