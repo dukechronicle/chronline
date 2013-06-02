4fcae6f34bb099d91607611b3860bb195dac4497
Jim Posen <jim.posen@gmail.com>
2013-06-02 16:27:06 -0700

    Added a few tests for Blog::Post.

--- spec/models/blog_post_spec.rb
+++ spec/models/blog_post_spec.rb
@@ -5,9 +5,13 @@ describe Blog::Post do
 
   subject { FactoryGirl.build(:blog_post) }
 
-  describe "#blog" do
-    before { subject.blog = Blog.find('playground') }
+  it { should belong_to(:author).class_name("Staff") }
+  it { should validate_presence_of(:author) }
 
+  describe "#blog" do
+    before { subject.blog = 'playground' }
+
+    it { should validate_presence_of(:blog) }
     its(:blog) { should be_a_kind_of(Blog) }
   end
 
