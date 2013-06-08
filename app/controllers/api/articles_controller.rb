66aaceac05f990844cd1d33af54ef1dc368a4e11
Jim Posen <jim.posen@gmail.com>
2013-06-08 12:14:39 -0700

    Enabled HTTP authentication for API.

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,4 +1,4 @@
-class Api::ArticlesController < ApplicationController
+class Api::ArticlesController < Api::BaseController
 
   def index
     taxonomy = Taxonomy.new("/#{params[:section]}/")
@@ -7,14 +7,14 @@ class Api::ArticlesController < ApplicationController
       .section(taxonomy)
       .order('published_at DESC')
       .paginate(page: 1, per_page: params[:limit])
-    render json: articles, include: :authors, methods: :thumb_square_s_url
+    respond_with articles, include: :authors, methods: :thumb_square_s_url
   end
 
   def search
     article_search = Article::Search.new(query: params[:query],
                                          sort: params[:sort],
                                          highlight: false)
-    render json: article_search.results
+    respond_with article_search.results
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -87,8 +87,8 @@ Chronline::Application.routes.draw do
     end
   end
 
-  constraints subdomain: 'api' do
-    namespace :api, path: '/' do
+  constraints subdomain: 'api', format: :json do
+    namespace :api, path: '/', defaults: {format: :json} do
       get 'qduke' => 'qduke#frontpage'
       get 'section/*section' => 'articles#index', as: :article_section
       get 'search' => 'articles#search'
