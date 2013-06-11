833b12efdea851dc150dbe3a90dab4f0810095fa
Jim Posen <jim.posen@gmail.com>
2013-06-11 00:13:38 -0700

    Newsletters can be scheduled to go out at a future time.

--- app/controllers/admin/newsletter_controller.rb
+++ app/controllers/admin/newsletter_controller.rb
@@ -9,14 +9,12 @@ class Admin::NewsletterController < Admin::BaseController
     params[:newsletter][:scheduled_time] = construct_time(params[:newsletter])
 
     if params[:newsletter].has_key?(:article)
-      message = "Article sent successfully"
       newsletter = ArticleNewsletter.new(params[:newsletter])
     else
-      message = "Newsletter sent successfully"
       newsletter = DailyNewsletter.new(params[:newsletter])
     end
 
-    newsletter.send_campaign
+    message = newsletter.send_campaign
     flash[:success] = message
     redirect_to admin_newsletter_path
   end
@@ -25,8 +23,11 @@ class Admin::NewsletterController < Admin::BaseController
   def construct_time(params)
     timezone = params.delete(:scheduled_timezone)
     if timezone.present?
-      # Rails form helper time inputs are named scheduled_time(1i)...
+      # Hour and minute are passed as scheduled_time(4i), scheduled_time(5i) by
+      # Rails form helper. Find next future date at which that time.
+      current_time = ActiveSupport::TimeZone[timezone].now
       date = 1.upto(5).map { |i| params.delete("scheduled_time(#{i}i)").to_i }
+      date[0..2] = [current_time.year, current_time.month, current_time.day]
       scheduled_time = ActiveSupport::TimeZone[timezone].local(*date)
       if scheduled_time.past?
         scheduled_time + 1.day
