cc15de5dd34df16a632c5e41d61fd366865151d2
Jim Posen <jim.posen@gmail.com>
2013-06-12 23:00:10 -0700

    Used ActionView helpers to generate HTML and used predefined photo_credit helper method.

--- app/models/article/embedded_media/image_tag.rb
+++ app/models/article/embedded_media/image_tag.rb
@@ -1,4 +1,8 @@
-class Article::EmbeddedMedia::ImageTag
+class Article::EmbeddedMedia::ImageTag < ActionView::Base
+  # HAX: there must be a better way to generate HTML
+  include ActionView::Helpers
+  include ImageHelper
+  include Rails.application.routes.url_helpers
 
   attr_reader :ids
 
@@ -18,15 +22,11 @@ class Article::EmbeddedMedia::ImageTag
   def to_html(class_objects)
     id = @ids[0]
     img = class_objects[id[:class]][id[:id]]
-    %Q(<span class="embedded-image">
-        <img src="#{img.original.url(:thumb_rect)}" />
-        <span class="photo-credit">
-          <a href="#{Rails.application.routes.url_helpers.site_staff_path(img.photographer)}">
-            #{img.photographer.name}
-          </a>
-        </span>
-      </span>
-    )
+    content_tag(:span, nil, class: 'embedded-image') do
+      photo_credit = photo_credit(img, link: true)
+      concat content_tag(:img, nil, src: img.original.url(:thumb_rect))
+      concat content_tag(:span, photo_credit, class: 'photo-credit')
+    end
   end
 
 end
