15fdb043f67afcb10fcd77ffda88727a1243f761
Prithvi Prabahar <prithvi923@gmail.com>
2013-06-19 21:08:43 -0700

    Remove conflict tag

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
