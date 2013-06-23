fd1cadfeecd511fd3624731941909489576699c8
Jim Posen <jim.posen@gmail.com>
2013-06-23 10:23:40 -0700

    Created route for viewing tagged posts in blog.

--- spec/models/blog_post_spec.rb
+++ spec/models/blog_post_spec.rb
@@ -8,6 +8,27 @@ describe Blog::Post do
   it { should belong_to(:author).class_name("Staff") }
   it { should validate_presence_of(:author) }
 
+  describe "::tagged_with" do
+    let(:blog_posts) do
+      [
+       FactoryGirl.create(:blog_post, tag_list: 'squirtle,charmander'),
+       FactoryGirl.create(:blog_post, tag_list: 'Squirtle,Charmander'),
+       FactoryGirl.create(:blog_post, tag_list: 'bulbasaur,charmander'),
+      ]
+    end
+
+    subject { Blog::Post.tagged_with('SQUIRTLE') }
+
+    it "should return case insensitive matches" do
+      should include(blog_posts[0])
+      should include(blog_posts[1])
+    end
+
+    it "should exclude posts without the tag" do
+      should_not include(blog_posts[2])
+    end
+  end
+
   describe "#blog" do
     before { subject.blog = 'playground' }
 
