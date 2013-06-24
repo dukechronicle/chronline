784de50d8e00805f04837bdb55de186af5918f75
Jim Posen <jim.posen@gmail.com>
2013-06-23 19:09:09 -0700

    Load schemata from JSON files.

--- lib/structured_data/schema.rb
+++ lib/structured_data/schema.rb
@@ -1,12 +1,17 @@
 module StructuredData
   class Schema
+    SCHEMA_PATH = %w(app models page schemata)
+
 
     def initialize(schema)
       @schema = schema
     end
 
     def self.[](schema_name)
-
+      path = SCHEMA_PATH + ["#{schema_name}.json"]
+      File.open(Rails.root.join(*path), 'r')  do |file|
+        self.new(JSON.load(file))
+      end
     end
 
     def json_schema
