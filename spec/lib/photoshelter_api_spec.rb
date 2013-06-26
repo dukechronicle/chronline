09d8757daca24af72ad72a12544786b8980da1bb
Prithvi Prabahar <prithvi923@gmail.com>
2013-06-25 23:18:04 -0700

    Add mocking for authentication and galleries

--- spec/lib/photoshelter_api_spec.rb
+++ spec/lib/photoshelter_api_spec.rb
@@ -1,35 +1,50 @@
 require 'spec_helper'
 
 describe PhotoshelterAPI do
+
   subject { PhotoshelterAPI.new(Settings.photoshelter.email, Settings.photoshelter.password) }
 
-  context "when constructed with an invalid email" do
+  context "when authenticated with an invalid email" do
     it "should raise an error" do
-      constructor = -> { PhotoshelterAPI.new("fail", Settings.photoshelter.password) }
+      constructor = -> { PhotoshelterAPI.new("fail", Settings.photoshelter.password).authenticate }
       constructor.should raise_error(StandardError)
     end
   end
 
-  context "when constructed with an invalid password" do
+  context "when authenticated with an invalid password" do
     it "should raise an error" do
-      constructor = -> { PhotoshelterAPI.new(Settings.photoshelter.email, "fail") }
+      constructor = -> { PhotoshelterAPI.new(Settings.photoshelter.email, "fail").authenticate }
       constructor.should raise_error(StandardError)
     end
   end
 
-  describe "#get_all_galleries" do
-    it "should return list of galleries"
+  describe "Authentication" do
+    use_vcr_cassette "authentication"
+
+    it "should authenticate properly" do
+      subject.authenticate.should == true
+    end
   end
 
-  describe "#get_gallery_images" do
+  describe "GET galleries" do
+    use_vcr_cassette "galleries"
+
+    it "should return list of galleries" do
+      # TODO: remove additional authentication here
+      subject.authenticate
+      subject.get_all_galleries.should be_a Array
+    end
+  end
+
+  describe "GET gallery images" do
     it "should return a list of images for a gallery"
   end
 
-  describe "#get_image_info" do
+  describe "GET image info" do
     it "should return information for an image"
   end
 
-  describe "#logout" do
+  describe "GET logout" do
     it "should logout and destroy the session"
   end
 end
