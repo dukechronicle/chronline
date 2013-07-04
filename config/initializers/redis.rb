4c3df5ed75c1952c2a63b51c48c122a5c0f9f9ec
Jim Posen <jim.posen@gmail.com>
2013-07-04 11:36:26 -0700

    Modified cropper to handle all images of same ratio.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 if Rails.env.test?
   $redis = MockRedis.new
 else
-  options = {driver: :hiredis}
+  options = { driver: :hiredis }
   options.merge!(Settings.redis)
   $redis = Redis.new(options)
 end
