40526a3ad02c4356b130c9f0b3366d0bd97a9a9b
Jim Posen <jim.posen@gmail.com>
2013-07-07 16:53:18 -0700

    Created sports event layout.

--- app/helpers/site/application_helper.rb
+++ app/helpers/site/application_helper.rb
@@ -10,7 +10,7 @@ EOS
   end
 
   def datetime_tag(datetime, format, options = {})
-    data = {format: format}
+    data = { format: format }
     data[:timestamp] = 'true' if options[:timestamp]
     content_tag(:time, nil, datetime: datetime.iso8601, class: options[:class],
                 data: data)
