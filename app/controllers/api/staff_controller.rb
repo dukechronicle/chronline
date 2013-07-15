8fb85b9dd50d7de52e30b56c2b6405f51610b7d4
Michael Lai <themichaellai@gmail.com>
2013-07-15 10:28:57 +0800

    Staff api controller/passing tests

--- app/controllers/api/staff_controller.rb
+++ app/controllers/api/staff_controller.rb
@@ -1,7 +1,35 @@
 class Api::StaffController < Api::BaseController
 
   def index
-    respond_with Staff.search(params[:search])
+    staff = Staff.paginate page: params[:page], per_page: params[:limit]
+    respond_with staff
   end
 
+  def show
+    staff = Staff.find params[:id]
+    respond_with staff
+  end
+
+  def create
+    staff = Staff.new params[:staff]
+    if staff.save
+      respond_with staff, status: :created, location: api_staff_index_url
+    else
+      head :bad_request, location: api_staff_index_url
+    end
+  end
+
+  def update
+    staff = Staff.find params[:id]
+    if staff.update_attributes params[:staff]
+      head :no_content
+    else
+      head :bad_request
+    end
+  end
+
+  def destroy
+    staff = Staff.find params[:id]
+    staff.destroy
+  end
 end
