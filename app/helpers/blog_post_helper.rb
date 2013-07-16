2eacb8461cee3cb2bd8bebbda426ea5c11628869
Jim Posen <jim.posen@gmail.com>
2013-07-15 21:15:46 -0700

    Use previous url as permanent url for blog posts.

--- app/helpers/blog_post_helper.rb
+++ app/helpers/blog_post_helper.rb
@@ -1,11 +1,15 @@
 module BlogPostHelper
 
   def permanent_blog_post_url(blog_post)
-    site_blog_post_url(blog_post.blog, blog_post.id, subdomain: :www)
+    if blog_post.previous_url.present?
+      blog_post.previous_url
+    else
+      site_blog_post_url(blog_post.blog, blog_post.id, subdomain: :www)
+    end
   end
 
   def blog_options
-    Blog.all.map {|blog| [blog.name, blog.id]}
+    Blog.all.map { |blog| [blog.name, blog.id] }
   end
 
   def recent_blog_posts(blog, limit=5)
