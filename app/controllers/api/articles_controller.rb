13783a76731db46e9a74d779394d903155b6b4e0
Michael Lai <themichaellai@gmail.com>
2013-08-02 09:45:46 +0800

    API article/image controllers/partial tests

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -17,4 +17,44 @@ class Api::ArticlesController < Api::BaseController
     respond_with article_search.results
   end
 
+  def show
+    article = Article.find params[:id]
+    respond_with article
+  end
+
+  def create
+    article = update_article(Article.new)
+    if article.save
+      respond_with article, status: :created, location: api_articles_url
+    else
+      head :bad_request
+    end
+  end
+
+  def update
+    @article = update_article(Article.find(params[:id]))
+    if @article.save
+      head :no_content
+    else
+      head :bad_request
+    end
+  end
+
+  def destroy
+    article = Article.find params[:id]
+    article.destroy
+  end
+
+  private
+
+  def update_article(article)
+    # Last element of taxonomy array may be an empty string
+    params[:article][:section].pop if params[:article][:section].last.blank?
+    author_names = params[:article].delete(:author_ids).reject {|s| s.blank? }
+    article.assign_attributes(params[:article])
+    article.authors = Staff.find_or_create_all_by_name(author_names)
+    article.published_at = DateTime.now if params[:article][:published_at].to_i == 1
+    article
+  end
+
 end
--- spec/requests/api/images_spec.rb
+++ spec/requests/api/images_spec.rb
@@ -2,34 +2,64 @@ require 'spec_helper'
 
 describe "Images API" do
 
-  describe "GET /images" do
+  describe "GET /images/*" do
+    let(:success) { 200 }
+    let(:no_response) { 204 }
+    subject { response }
+
     before do
       stub_request(:put, /#{Settings.aws.bucket}\.s3\.amazonaws\.com/)
       @image = FactoryGirl.create(:image)
-      get api_images_url(subdomain: :api)
     end
 
-    let(:success) { 200 }
-    let(:images) { JSON.parse(response.body) }
-
-    it { response.status.should be(success) }
-    it { images.should be_an(Array) }
-    it { images.should have(1).images }
-
-    it "should have image properties" do
-      attrs = @image.attributes
-      # Timestamps are in different format for JSON
-      attrs.each do |key, value|
-        if value.respond_to?(:iso8601)
-          attrs[key] = value.iso8601
-        end
+    describe "get all images" do
+      before do
+        get api_images_url(subdomain: :api)
       end
-      images.first.should include(attrs)
+      let(:images) { JSON.parse(response.body) }
+
+      it { response.status.should be(success) }
+      it { images.should be_an(Array) }
+      it { images.should have(1).images }
+
+      it "should have image properties" do
+        attrs = @image.attributes
+        # Timestamps are in different format for JSON
+        attrs.each do |key, value|
+          if value.respond_to?(:iso8601)
+            attrs[key] = value.iso8601
+          end
+        end
+        images.first.should include(attrs)
+      end
+
+      it "should have original url" do
+        images.first.should include(
+          "thumbnail_url" => @image.original.url(:thumb_rect))
+      end
     end
 
-    it "should have original url" do
-      images.first.should include(
-        "thumbnail_url" => @image.original.url(:thumb_rect))
+    describe "get single image" do
+      before { get api_image_url(subdomain: :api, id: @image.id) }
+      let(:image) { JSON.parse(response.body) }
+
+      its(:status) { should == success }
+      it "should have have image properties" do
+        attrs = @image.attributes
+        # Timestamps are in different format for JSON
+        attrs.each do |key, value|
+          if value.respond_to?(:iso8601)
+            attrs[key] = value.iso8601
+          end
+        end
+        image.should include(attrs)
+      end
     end
+  end
+  describe "DELETE /image/*" do
+    let(:res) { JSON.decode(response.body) }
+    subject { response }
+    let!(:staff) { FactoryGirl.create :staff }
+    it { change(Staff, :count).by(1) }
   end
 end
