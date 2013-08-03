874360337c6c47eae29f4fbcc04e7ebc1b88d98f
Jim Posen <jim.posen@gmail.com>
2013-08-03 15:27:30 -0400

    Global search searches all models.

--- app/controllers/site/searches_controller.rb
+++ app/controllers/site/searches_controller.rb
@@ -3,10 +3,11 @@ class Site::SearchesController < Site::BaseController
   def show
     if params[:search]
       params[:search][:page] = params[:page]
+      params[:search][:highlight] = true
       @search = Search.new(params[:search])
       @results = @search.results
     else
-      @search = Search.new(highlight: true)
+      @search = Search.new
       @results = []
     end
   end
