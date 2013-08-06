e50ca0d984ea8cfb2ffb3547ae358da5d849503c
Jim Posen <jim.posen@gmail.com>
2013-08-06 17:25:43 -0400

    Staff controller specs test HTTP authentication and more.

--- spec/requests/api/staff_spec.rb
+++ spec/requests/api/staff_spec.rb
@@ -1,103 +1,147 @@
 require 'spec_helper'
 
-describe Api::StaffController do
-  let(:success) { 200 }
-  let(:created) { 201 }
-  let(:bad_request) { 400 }
-  let(:no_response) { 204 }
+describe "/staff/*" do
+  before(:all) { @user = FactoryGirl.create(:user) }
+  after(:all) { @user.destroy }
+  subject { ActiveSupport::JSON.decode(response.body) }
 
-  describe "GET /staff/*" do
+  describe "GET /staff" do
     let!(:orig_staff) { FactoryGirl.create_list :staff, 30 }
-    let(:res) { ActiveSupport::JSON.decode(response.body) }
-    subject { response }
 
-    describe "list staff" do
+    context "when page 1 is fetched" do
       before { get api_staff_index_url(subdomain: :api, page: 1) }
 
-      its(:status) { should == success }
-      it { res.should have(25).staff }
+      it { response.status.should == Rack::Utils.status_code(:ok) }
+      it { should have(25).staff }
     end
 
-    describe "get a staff" do
-      before { get api_staff_url(subdomain: :api, id: orig_staff[0].id) }
+    context "when page 2 is fetched" do
+      before { get api_staff_index_url(subdomain: :api, page: 2) }
 
-      its(:status) { should == success }
-      it "should have staff attributes" do
-        attrs = json_attributes orig_staff[0]
-        res.should include(attrs)
-      end
+      it { response.status.should == Rack::Utils.status_code(:ok) }
+      it { should have(5).staff }
     end
   end
 
-  describe "POST /staff/" do
-    let(:res) { ActiveSupport::JSON.decode(response.body).symbolize_keys }
-    subject { response }
-    describe "create valid staff" do
-      let(:new_staff_attrs) { FactoryGirl.attributes_for :staff }
-      before { post api_staff_index_url(subdomain: :api), staff: new_staff_attrs }
+  describe "GET /staff/:id" do
+    before { get api_staff_url(orig_staff, subdomain: :api) }
+    let!(:orig_staff) { FactoryGirl.create :staff }
 
-      its(:status) { should == created }
+    it { response.status.should == Rack::Utils.status_code(:ok) }
+
+    it "should have staff attributes" do
+      attrs = ActiveSupport::JSON.decode(orig_staff.to_json)
+      should include(attrs)
+    end
+  end
+
+  describe "POST /staff" do
+    let(:new_staff_attrs) { FactoryGirl.attributes_for :staff }
+
+    describe "when user is not authenticated" do
+      before do
+        post api_staff_index_url(subdomain: :api), staff: new_staff_attrs
+      end
+
+      it { response.status.should == Rack::Utils.status_code(:unauthorized) }
+    end
+
+    describe "when staff is valid" do
+      before do
+        post api_staff_index_url(subdomain: :api), { staff: new_staff_attrs },
+          { 'HTTP_AUTHORIZATION' => http_auth(@user) }
+      end
+
+      it { response.status.should == Rack::Utils.status_code(:created) }
+
       it "should have the same fields as the original" do
-        res.should include(new_staff_attrs)
+        should include(new_staff_attrs.stringify_keys)
       end
+
       it "should have an integer id" do
-        res[:id].should be_a_kind_of(Integer)
+        subject['id'].should be_a_kind_of(Integer)
       end
-      it "should include updated_at and created_at" do
-        res.should include(:updated_at, :created_at)
-      end
-      it { change(Staff, :count).by(1) }
+
+      it { should include('updated_at', 'created_at') }
+      it { Staff.should have(1).record }
     end
 
-    describe "create invalid staff" do
-      before { post api_staff_index_url(subdomain: :api), staff: {} }
-      its(:status) { should == bad_request }
+    describe "when staff is invalid" do
+      before do
+        new_staff_attrs.delete(:name)
+        post api_staff_index_url(subdomain: :api), { staff: new_staff_attrs },
+          { 'HTTP_AUTHORIZATION' => http_auth(@user) }
+      end
+
+      it { response.status.should == Rack::Utils.status_code(:bad_request) }
+      it "should respond with validation errors" do
+        should include('name')
+      end
     end
   end
 
-  describe "PUT /staff/*" do
-    let(:res) { ActiveSupport::JSON.decode(response.body) }
-    subject { response }
-    let!(:staff_attrs) { FactoryGirl.attributes_for :staff }
+  describe "PUT /staff/:id" do
+    let(:staff_attrs) { FactoryGirl.attributes_for :staff }
     let!(:staff) { FactoryGirl.create :staff, staff_attrs }
-    describe "update staff with valid data" do
-      let(:updated_attrs) do
-        new_staff_attrs = staff_attrs.clone
-        new_staff_attrs[:name] = 'Ash Ketchup'
-        new_staff_attrs
-      end
-      before { put api_staff_url(subdomain: :api, id: staff.id, staff: updated_attrs) }
 
-      its(:status) { should == no_response }
-      it "should have a changed name" do
-        staff.reload
-        staff.name.should eq(updated_attrs[:name])
+    describe "when user is not authenticated" do
+      before { delete api_staff_url(staff, subdomain: :api) }
+      it { response.status.should == Rack::Utils.status_code(:unauthorized) }
+    end
+
+    describe "when update data is valid" do
+      let(:updated_attrs) { { name: 'Pokefan Harold' } }
+
+      before do
+        put api_staff_url(subdomain: :api, id: staff.id),
+          { staff: updated_attrs }, { 'HTTP_AUTHORIZATION' => http_auth(@user) }
       end
-      it { change(Staff, :count).by(0) }
+
+      it { response.status.should == Rack::Utils.status_code(:no_content) }
+
+      it "should have updated attributes" do
+        staff.reload.name.should == updated_attrs[:name]
+      end
+
+      it "should not create or destroy any records" do
+        Staff.should have(1).record
+      end
     end
 
     describe "update staff with invalid data" do
-      let(:invalid_attrs) { {name: '' } }
-      before { put api_staff_url subdomain: :api, id: staff.id, staff: invalid_attrs }
-      its(:status) { should == bad_request }
+      let(:invalid_attrs) { { name: '' } }
+
+      before do
+        put api_staff_url(subdomain: :api, id: staff.id),
+          { staff: invalid_attrs }, { 'HTTP_AUTHORIZATION' => http_auth(@user) }
+
+        it { response.status.should == Rack::Utils.status_code(:bad_request) }
+        it "should respond with validation errors" do
+          should include('name')
+        end
+      end
     end
   end
 
-  describe "DELETE /staff/*" do
-    let(:res) { ActiveSupport::JSON.decode(response.body) }
-    subject { response }
+  describe "DELETE /staff/:id" do
     let!(:staff) { FactoryGirl.create :staff }
-    it { change(Staff, :count).by(1) }
-  end
-end
 
-def json_attributes(model)
-  attrs = model.attributes
-  # Timestamps are in different format for JSON
-  attrs.each do |key, value|
-    if value.respond_to?(:iso8601)
-      attrs[key] = value.iso8601
+    describe "when user is not authenticated" do
+      before { delete api_staff_url(staff, subdomain: :api) }
+      it { response.status.should == Rack::Utils.status_code(:unauthorized) }
+    end
+
+    describe "when user is authenticated" do
+      before do
+        delete api_staff_url(staff, subdomain: :api), nil,
+          { 'HTTP_AUTHORIZATION' => http_auth(@user) }
+      end
+
+      it { response.status.should == Rack::Utils.status_code(:no_content) }
+
+      it "should remove the staff record" do
+        Staff.should have(:no).records
+      end
     end
   end
-  attrs
 end
