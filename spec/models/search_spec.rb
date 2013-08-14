6b793214503650306f4707af7e4ac8bee6472440
Jim Posen <jim.posen@gmail.com>
2013-08-13 23:32:51 -0400

    Started writing search spec.

---
+++ spec/models/search_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe Search do
+  describe "#initialize" do
+    context "when initialized with model parameter" do
+      it "should extend model specific search module" do
+        search = Search.new(model: Article)
+        search.should be_a(Article::Search)
+        search.should respond_to(:section)
+      end
+
+      it "should accept model class name as string" do
+        search = Search.new(model: 'Article')
+        search.should be_a(Article::Search)
+        search.model.should == Article
+      end
+    end
+
+    it "should use default values for options" do
+      today = DateTime.new(1998, 9, 30)
+      DateTime.stub(:now).and_return(today)
+      search = Search.new
+
+      search.page.should == 1
+      search.per_page.should == 25
+      search.sort.should == :score
+      search.order.should == :desc
+      search.highlight.should == false
+      search.end_date.should == today
+    end
+
+    it "should accept attributes" do
+      search = Search.new(page: 2)
+      search.page.should == 2
+    end
+  end
+
+  describe "#results" do
+    let(:search) { Search.new(query: 'jhoto') }
+    subject { search.results }
+
+    context "when query is blank" do
+      let(:search) { Search.new(query: '') }
+      it { should be_nil }
+    end
+
+    it "should be paginatable" do
+      subject.total_pages.should be_an(Integer)
+    end
+
+    it "should cache solr request" do
+      search.results
+      Sunspot.stub(:search)  # Start spying on search method
+      search.results
+      Sunspot.should_not have_received(:search)
+    end
+
+    context "when highlight option is on" do
+      it "should wrap highlighted elements with <mark> tags"
+    end
+  end
+end
