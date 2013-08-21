9fe9d5020755943ce773e5082fb5ebe20b075734
Michael Lai <themichaellai@gmail.com>
2013-08-20 23:32:55 -0400

    Separate GET actions in Images API spec

--- spec/requests/api/images_spec.rb
+++ spec/requests/api/images_spec.rb
@@ -3,60 +3,61 @@ require 'spec_helper'
 describe "Images API" do
   before(:all) { @user = FactoryGirl.create(:user) }
   after(:all) { @user.destroy }
-  describe "GET /images/*" do
+
+  describe "GET /images" do
     subject { response }
 
     before do
       stub_request(:put, /#{Settings.aws.bucket}\.s3\.amazonaws\.com/)
       @image = FactoryGirl.create(:image)
+      get api_images_url(subdomain: :api)
+    end
+    let(:images) { JSON.parse(response.body) }
+
+    its(:status) { should be(Rack::Utils.status_code(:ok)) }
+    it { images.should be_an(Array) }
+    it { images.should have(1).images }
+
+    it "should have image properties" do
+      attrs = json_attributes(@image)
+      images.first.should include(attrs)
     end
 
-    describe "get all images" do
-      before do
-        get api_images_url(subdomain: :api)
-      end
-      let(:images) { JSON.parse(response.body) }
-
-      it { response.status.should be(Rack::Utils.status_code(:ok)) }
-      it { images.should be_an(Array) }
-      it { images.should have(1).images }
-
-      it "should have image properties" do
-        attrs = json_attributes(@image)
-        images.first.should include(attrs)
-      end
-
-      it "should have thumbnail url" do
-        images.first.should include(
-          "thumbnail_url" => @image.thumbnail_url)
-      end
+    it "should have thumbnail url" do
+      images.first.should include(
+        "thumbnail_url" => @image.thumbnail_url)
     end
+  end
 
-    describe "get single image" do
-      before { get api_image_url(subdomain: :api, id: @image.id) }
-      let(:image) { JSON.parse(response.body) }
-      let(:res) { ActiveSupport::JSON.decode(response.body) }
+  describe "GET /images/:id" do
+    before do
+      stub_request(:put, /#{Settings.aws.bucket}\.s3\.amazonaws\.com/)
+      @image = FactoryGirl.create(:image)
+      get api_image_url(@image, subdomain: :api)
+    end
+    subject { response }
 
-      its(:status) { should == Rack::Utils.status_code(:ok) }
-      it "should have have image properties" do
-        attrs = json_attributes(@image)
-        image.should include(attrs)
-      end
-      it "should match Camayak spec" do
-        res.should include(
-          'caption' => @image.caption,
-          'location' => @image.location,
-          'credit' => @image.credit,
-          'original_file_name' => @image.original_file_name,
-          'original_content_type' => @image.original_content_type,
-          'original_file_size' => @image.original_file_size,
-          'original_updated_at' => @image.original_updated_at.iso8601,
-          'created_at' => @image.created_at.iso8601,
-          'updated_at' => @image.updated_at.iso8601,
-          'photographer_id' => @image.photographer_id,
-          'published_url' => @image.original.url
-        )
-      end
+    let(:res) { ActiveSupport::JSON.decode(response.body) }
+
+    its(:status) { should == Rack::Utils.status_code(:ok) }
+    it "should have have image properties" do
+      attrs = json_attributes(@image)
+      res.should include(attrs)
+    end
+    it "should match Camayak spec" do
+      res.should include(
+        'caption' => @image.caption,
+        'location' => @image.location,
+        'credit' => @image.credit,
+        'original_file_name' => @image.original_file_name,
+        'original_content_type' => @image.original_content_type,
+        'original_file_size' => @image.original_file_size,
+        'original_updated_at' => @image.original_updated_at.iso8601,
+        'created_at' => @image.created_at.iso8601,
+        'updated_at' => @image.updated_at.iso8601,
+        'photographer_id' => @image.photographer_id,
+        'published_url' => @image.original.url
+      )
     end
   end
 
