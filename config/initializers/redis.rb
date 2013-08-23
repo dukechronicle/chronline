fdcdea8ba50c00a8b7cd348b264e28f74e186830
Jim Posen <jim.posen@gmail.com>
2013-08-23 12:03:19 -0400

    Fixed Postgres forking in Resque workers.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,4 +5,15 @@ else
   options.merge!(Settings.redis)
   $redis = Redis.new(options)
   Resque.redis = $redis
+
+  # https://devcenter.heroku.com/articles/forked-pg-connections
+  Resque.before_fork do
+    defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+  end
+
+  Resque.after_fork do
+    defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+  end
 end
