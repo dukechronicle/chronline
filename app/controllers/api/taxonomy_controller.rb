73a5e3621b29b22d80d09dcaf6998f73f1d16ffb
Jim Posen <jim.posen@gmail.com>
2013-08-26 22:01:49 -0400

    Blog pseudo-section nodes are returned by taxonomy controller.

--- app/controllers/api/taxonomy_controller.rb
+++ app/controllers/api/taxonomy_controller.rb
@@ -3,11 +3,13 @@ require 'will_paginate/array'
 class Api::TaxonomyController < Api::BaseController
 
   def index
-    sections = Taxonomy.nodes.paginate(
+    sections = Taxonomy.nodes
+    blogs = Blog.nodes
+    nodes = (sections + blogs).paginate(
       page: params[:page],
       per_page: params[:limit]
     )
-    render json: sections
+    render json: nodes
   end
 
 end
