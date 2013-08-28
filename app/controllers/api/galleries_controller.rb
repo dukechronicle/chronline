b22db67c28c2cf52bbd01fde68155c8f237abf4d
Prithvi Prabahar <prithvi923@gmail.com>
2013-08-28 14:00:07 -0400

    Add gallery images route

--- app/controllers/api/galleries_controller.rb
+++ app/controllers/api/galleries_controller.rb
@@ -7,4 +7,10 @@ class Api::GalleriesController < ApplicationController
     render json: @api.get_all_galleries
   end
 
+  def show
+    @api = PhotoshelterAPI.new(Settings.photoshelter.email, Settings.photoshelter.password)
+    @api.authenticate
+    render json: @api.get_gallery_images(params[:gallery_id])
+  end
+
 end
