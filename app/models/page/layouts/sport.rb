c855ce9c79b0364b4009bae4e63422f38ede77ba
Jim Posen <jim.posen@gmail.com>
2013-09-01 15:27:44 -0400

    Game score showed for sport page.

--- app/models/page/layouts/sport.rb
+++ app/models/page/layouts/sport.rb
@@ -41,51 +41,6 @@ class Page::Layouts::Sport < Layout
         'required' => true,
         'label' => 'Game Information',
         'properties' => {
-          'sport' => {
-            'type' => 'string',
-            'required' => true,
-          },
-          'home_game' => {
-            'type' => 'boolean',
-            'required' => true,
-          },
-          'main_team' => {
-            'type' => 'object',
-            'required' => true,
-            'description' => 'This will usually be Duke',
-            'properties' => {
-              'name' => {
-                'type' => 'string',
-                'required' => true,
-              },
-              'mascot' => {
-                'type' => 'string',
-                'required' => true,
-              },
-              'logo' => {
-                'type' => 'string',
-                'required' => true,
-              }
-            }
-          },
-          'opposing_team' => {
-            'type' => 'object',
-            'required' => true,
-            'properties' => {
-              'name' => {
-                'type' => 'string',
-                'required' => true,
-              },
-              'mascot' => {
-                'type' => 'string',
-                'required' => true,
-              },
-              'logo' => {
-                'type' => 'string',
-                'required' => true,
-              }
-            }
-          },
           'date' => {
             'required' => true,
             'extends' => datetime_schema,
@@ -95,6 +50,10 @@ class Page::Layouts::Sport < Layout
             'required' => true,
           },
           'channel' => {
+            'type' => 'string',
+            'required' => true,
+          },
+          'espn_url' => {
             'type' => 'string',
             'required' => true,
           }
