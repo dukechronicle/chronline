5e34238ee90faeccd836d26b649bf4b16989d3b3
Michael Lai <themichaellai@gmail.com>
2013-09-17 18:44:42 -0400

    Added Youtube tag

---
+++ app/models/post/embedded_media/youtube_tag.rb
@@ -0,0 +1,17 @@
+class Post
+  class EmbeddedMedia
+    class YoutubeTag < ActionView::Base
+      include ActionView::Helpers
+
+      def initialize(_embedded_media, url)
+        @youtube_id = /v=([a-zA-Z0-9_-]{11})/.match(url)[1]
+      end
+
+      def to_html(float: :right)
+        content_tag(
+          :iframe, nil, frameborder: 0, allowfullscreen: 'yes',
+          src: "//www.youtube.com/embed/#{@youtube_id}?showinfo=0&rel=0")
+      end
+    end
+  end
+end
