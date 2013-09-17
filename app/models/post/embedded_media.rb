6860bbfd6ab3989b31a131a526459a83c8f8191c
Michael Lai <themichaellai@gmail.com>
2013-09-17 18:44:42 -0400

    Added Youtube tag

--- app/models/post/embedded_media.rb
+++ app/models/post/embedded_media.rb
@@ -7,20 +7,22 @@ class Post
     end
 
     def render
-      tag_list = @body.scan(/{{([a-zA-Z]*):([\w\,\-]*)}}/)
+      tag_list = @body.scan(/{{([a-zA-Z]*):(\S*?)}}/)
       tags = tag_list.map do |tag, data|
         case tag
         when 'Image'
           ImageTag.new(self, *data.split(','))
         when 'Icon'
           IconTag.new(self, *data.split(','))
+        when 'Youtube'
+          YoutubeTag.new(self, *data.split(','))
         end
       end
 
       execute_queries
 
       float_left = true
-      @rendered = @body.gsub(/{{([a-zA-Z]*):([\w\,\-]*)}}/) do |t|
+      @rendered = @body.gsub(/{{([a-zA-Z]*):(\S*?)}}/) do |t|
         float_left = !float_left
         tags.shift.to_html(float: float_left ? :left : :right)
       end
