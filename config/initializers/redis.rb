60921fae884f483335c9c5e0a8fc4ae3152ea938
Prithvi Prabahar <prithvi923@gmail.com>
2013-10-30 11:09:23 -0400

    Fix Redis settings

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 if Rails.env.test?
   $redis = MockRedis.new
 else
-  Resque.redis = $redis = Redis.new(url: Settings.redis, driver: :hiredis)
+  Resque.redis = $redis = Redis.new(url: Settings.redis.url, driver: :hiredis)
 
   # https://devcenter.heroku.com/articles/forked-pg-connections
   Resque.before_fork do
