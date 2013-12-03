059f894e177717e9b91bac7fd6b503ab85ef5237
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-12-03 18:46:41 -0500

    added comments

--- app/models/photoshelter_image.rb
+++ app/models/photoshelter_image.rb
@@ -8,10 +8,12 @@ class PhotoshelterImage < ActiveRecord::Base
     "http://cdn.c.photoshelter.com/img-get/#{pid}/"
   end
 
+  # url of the photoshelter buy page
   def get_photoshelter_url
     "http://dukechronicle.photoshelter.com/gallery-image/#{get_gallery.get_slug}/#{get_gallery.gid}/#{pid}"
   end
 
+  # gets the gallery by gallery id 
   def get_gallery
     Gallery.where(gid: gid).first
   end
