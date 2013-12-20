2ee09b49153e3baf87535e9327d73fb5d13331ec
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-12-20 17:15:26 -0500

    basics of gallery show page

--- app/controllers/site/galleries_controller.rb
+++ app/controllers/site/galleries_controller.rb
@@ -11,8 +11,8 @@ class Site::GalleriesController < Site::BaseController
   end
 
   def show
-    gallery = Gallery.find_by_gid(params[:id])
-    @images = gallery.get_gallery_images
+    @gallery = Gallery.find_by_gid(params[:id])
+    #@images = gallery.get_gallery_images
   end
 
 end
