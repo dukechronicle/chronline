ce0c69e7cce14ade5acada405cd2a9c6fff1ee08
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-12-29 15:24:41 -0500

    updated admin gallery to include description and added linking from photag to staff page on gallery show page

---
+++ app/helpers/gallery_helper.rb
@@ -0,0 +1,14 @@
+module GalleryHelper
+
+  def byline(gallery)
+    photag_string = gallery.credit.split('/')[0].strip
+    photag = get_staff_from_photag(photag_string)
+    link_to photag.name, images_site_staff_path(photag)
+  end
+
+  def get_staff_from_photag(photag)
+    names = []
+    names << photag
+    photag = Staff.find_or_create_all_by_name(names)[0]
+  end
+end
