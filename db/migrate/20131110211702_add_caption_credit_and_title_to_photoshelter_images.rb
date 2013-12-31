7f8a9652e62b3324e198f2b47c4f55a30207f49b
Jim Posen <jim.posen@gmail.com>
2013-12-31 15:03:24 -0800

    Consolidate all photoshelter-related migrations.

--- db/migrate/20131110211702_add_caption_credit_and_title_to_photoshelter_images.rb
+++
@@ -1,7 +0,0 @@
-class AddCaptionCreditAndTitleToPhotoshelterImages < ActiveRecord::Migration
-  def change
-    add_column :photoshelter_images, :caption, :text
-    add_column :photoshelter_images, :credit, :string
-    add_column :photoshelter_images, :title, :string
-  end
-end
