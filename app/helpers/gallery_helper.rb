047eace7ab56503b2253c533d701b1c30445e197
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-12-30 19:44:34 -0500

    renamed conflicting byline and fixed gallery size

--- app/helpers/gallery_helper.rb
+++ app/helpers/gallery_helper.rb
@@ -1,6 +1,6 @@
 module GalleryHelper
 
-  def byline(gallery)
+  def photag_byline(gallery)
     photag_string = gallery.credit.split('/')[0].strip
     photag = get_staff_from_photag(photag_string)
     link_to photag.name, images_site_staff_path(photag)
