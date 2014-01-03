6c0e39349fcf3290661b84e4f41fe8a9cdfa1014
Roger <rogerzou94@gmail.com>
2014-01-02 22:07:56 -0500

    crude but functional working site interface

--- app/controllers/site/responses_controller.rb
+++ app/controllers/site/responses_controller.rb
@@ -1,5 +1,14 @@
 class Site::ResponsesController < Site::BaseController
 
+	def create
+		@response = Response.new(params[:response])
+		if @response.save
+			redirect_to :back
+		else
+			redirect_to :back
+		end
+	end
+
 	def destroy
 		@response = Response.find(params[:id])
 		@response.destroy
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,8 +2,15 @@ class Response < ActiveRecord::Base
 
 		belongs_to :topic
 
-		attr_accessible :content
+		attr_accessible :content, :topic_id
 
 		validates :content, presence: true, length: { maximum: 140 }
 
+		before_save :set_votes
+
+		def set_votes
+			self.upvotes = 0
+			self.downvotes = 0
+		end
+		
 end
