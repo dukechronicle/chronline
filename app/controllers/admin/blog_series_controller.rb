d484d9585cc7db5745e7f31d094e69f069f15962
Jim Posen <jim.posen@gmail.com>
2014-01-02 23:57:00 -0500

    Implemented admin interface for blog series.

--- app/controllers/admin/blog_series_controller.rb
+++ app/controllers/admin/blog_series_controller.rb
@@ -2,4 +2,36 @@ class Admin::BlogSeriesController < Admin::BaseController
   def index
     @blog_series = Blog::Series.page(params[:page])
   end
+
+  def new
+    @blog_series = Blog::Series.new
+  end
+
+  def create
+    @blog_series = Blog::Series.new(params[:blog_series])
+    if @blog_series.save
+      redirect_to admin_blog_series_index_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @blog_series = Blog::Series.find(params[:id])
+  end
+
+  def update
+    @blog_series = Blog::Series.find(params[:id])
+    if @blog_series.update_attributes(params[:blog_series])
+      redirect_to site_blog_tagged_url(
+        @blog_series.blog, @blog_series.name, subdomain: 'www')
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @blog_series = Blog::Series.find(params[:id])
+    @blog_series.destroy
+  end
 end
--- app/models/blog/series.rb
+++ app/models/blog/series.rb
@@ -1,6 +1,12 @@
 class Blog::Series < ActiveRecord::Base
   self.table_name = :blog_series
 
+  attr_accessible :blog_id, :image_id, :name
+
+  # HAX: needed so that url_for works correctly for blog posts
+  # self.model_name.instance_variable_set(:@singular_route_key, 'series')
+  # self.model_name.instance_variable_set(:@route_key, 'series')
+
   belongs_to :image
   belongs_to :tag, class_name: 'ActsAsTaggableOn::Tag'
 
@@ -8,7 +14,13 @@ class Blog::Series < ActiveRecord::Base
   validates :image, presence: true
   validates :tag, presence: true
 
-  delegate :name, to: :tag
+  def name
+    tag.try(:name)
+  end
+
+  def name=(name)
+    self.tag = ActsAsTaggableOn::Tag.find_or_create_with_like_by_name(name)
+  end
 
   def blog
     Blog.find_by_id(blog_id)
