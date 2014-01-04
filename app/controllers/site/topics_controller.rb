2262013efd72760f9e6806dd09c0991d8b42335b
Roger <rogerzou94@gmail.com>
2014-01-04 14:51:14 -0500

    problem with :admin show

--- app/controllers/site/topics_controller.rb
+++ app/controllers/site/topics_controller.rb
@@ -2,8 +2,7 @@ class Site::TopicsController < Site::BaseController
 
 	def show
 		@topic = Topic.find(params[:id])
-		@responses = @topic.responses.where('reported = ? OR approved = ?', false, true).order('created_at DESC')
-		@responses = @responses.paginate(page: params[:page], per_page: 5)
+		@responses = @topic.responses.where('reported = ? OR approved = ?', false, true).order('created_at DESC').paginate(page: params[:page], per_page: 5)
 	end
 
 	def index
