ac7a70cec5f90e9ea029c66d1933d7f55629a2cb
Roger <rogerzou94@gmail.com>
2014-01-06 17:32:26 -0500

    improve blacklist, limit votes

--- app/controllers/site/topic_responses_controller.rb
+++ app/controllers/site/topic_responses_controller.rb
@@ -1,7 +1,7 @@
 class Site::TopicResponsesController < Site::BaseController
 
 	def create
-		@response = Topic.find(params[:topic_id]).responses.build(params[:topic_response]) 
+		@response = Topic.find(params[:topic_id]).responses.build(params[:topic_response])
 		if @response.save
 			# some message?
 		end
@@ -10,13 +10,23 @@ class Site::TopicResponsesController < Site::BaseController
 
 	def upvote
 		@response = Topic::Response.find(params[:id])
-		@response.update_attributes(upvotes: @response.upvotes + 1)
+		status = session_upvote_status(@response.id)
+		if status == 1
+			@response.update_attributes(upvotes: @response.upvotes + 1)
+		elsif status == -1
+			@response.update_attributes(upvotes: @response.upvotes - 1)
+		end
 		redirect_to :back
 	end
 
 	def downvote
 		@response = Topic::Response.find(params[:id])
-		@response.update_attributes(downvotes: @response.downvotes + 1)
+		status = session_downvote_status(@response.id)
+		if status == 1
+			@response.update_attributes(downvotes: @response.downvotes + 1)
+		elsif status == -1
+			@response.update_attributes(downvotes: @response.downvotes - 1)
+		end
 		redirect_to :back
 	end
 
@@ -25,5 +35,46 @@ class Site::TopicResponsesController < Site::BaseController
 		@response.destroy
 		redirect_to :back
 	end
+
+	private
+
+		def session_upvote_status(response_id)
+			session_vote_helper(response_id, 0)
+		end
+
+		def session_downvote_status(response_id)
+			session_vote_helper(response_id, 1)
+		end
+
+		def session_vote_helper(response_id, up_or_down)
+
+			if session[:votes].nil?
+				session[:votes] = { response_id => [false,false] }
+			elsif !session[:votes].has_key?(response_id)
+				session[:votes].merge! response_id => [false,false]
+			end
+
+			if up_or_down == 0
+				this = 0
+				other = 1
+			elsif up_or_down == 1
+				this = 1
+				other = 0
+			end
+
+			votes = session[:votes][response_id]
+			if votes[this] == false
+				if votes[other] == false
+					votes[this] = true
+					return 1
+				elsif votes[other] = true
+					return 0
+				end
+			elsif votes[this] == true
+				votes[this] = false
+				return -1
+			end
+
+		end
 
 end
