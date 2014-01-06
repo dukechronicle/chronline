d21722d0914addb32bb8cc40a06b19df96f0c997
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-01-05 21:11:55 -0500

    nav bar updated with links, white background for titles, thumbnail titles are links and spacing reduced, masonry with 6 galleries

--- app/controllers/site/galleries_controller.rb
+++ app/controllers/site/galleries_controller.rb
@@ -12,7 +12,7 @@ class Site::GalleriesController < Site::BaseController
   def show
     @gallery = Gallery.find_by_gid(params[:id])
     #@images = gallery.get_gallery_images
-    @recent = Gallery.order('created_at DESC').limit(12)
+    @recent = Gallery.order('created_at DESC').limit(6)
   end
 
 end
