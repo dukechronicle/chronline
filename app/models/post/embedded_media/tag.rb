abeae762bb46645b12e60fa7005e2fdb7f5a60fb
Michael Lai <themichaellai@gmail.com>
2014-01-06 16:58:36 -0500

    Move tag matching to the Tag classes

--- app/models/post/embedded_media/tag.rb
+++ app/models/post/embedded_media/tag.rb
@@ -1,6 +1,11 @@
 class Post
   class EmbeddedMedia
     class Tag < ActionView::Base
+      Dir[File.join(Rails.root, "app", "models", "post", "embedded_media", "**",
+                    "*.rb")].each do |file|
+        require_dependency file
+      end
+
       def self.parse_tag(tag)
         match = /{{([a-zA-Z]*):([^\}]*?)}}/.match(tag)
         if match.present? and match[1].eql? self.name.demodulize.sub(/Tag$/, '')
