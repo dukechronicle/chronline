c559c23c3dc0407b5680fe84d12b745f33dfd32e
Jim Posen <jim.posen@gmail.com>
2014-01-08 18:30:39 -0500

    Extra validation for pages.

--- lib/layout_validator.rb
+++ lib/layout_validator.rb
@@ -2,6 +2,11 @@ class LayoutValidator < ActiveModel::Validator
 
   def validate(record)
     record.errors[:layout_data] += record.layout.validate
+    begin
+      record.layout.generate_model
+    rescue
+      record.errors[:layout_data] << "Something went wrong. Try again."
+    end
   end
 
 end
