3fd4a2f41b84263cbba11f0976018b8847d0ce5f
Michael Lai <themichaellai@gmail.com>
2014-01-24 00:31:57 -0500

    Added new/edit poll form

--- app/controllers/admin/polls_controller.rb
+++ app/controllers/admin/polls_controller.rb
@@ -8,11 +8,32 @@ class Admin::PollsController < Admin::BaseController
   end
 
   def create
-    @poll = Poll.new(params[:poll])
+    @poll = update_poll(Poll.new)
     if @poll.save
-      redirect_to admin_poll_url(@poll, subdomain: :admin)
+      redirect_to edit_admin_poll_url(@poll, subdomain: :admin)
     else
       render 'new'
     end
+  end
+
+  def edit
+    @poll = Poll.find(params[:id])
+  end
+
+  def update
+    @poll = update_poll(Poll.find(params[:id]))
+    if @poll.save
+      redirect_to edit_admin_poll_url(@poll, subdomain: :admin)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def update_poll(poll)
+    choice_titles = params[:poll].delete(:choice_ids).reject {|s| s.blank? }
+    poll.assign_attributes(params[:poll])
+    poll.choices = Poll::Choice.find_create_or_delete_poll_choices(poll, choice_titles)
+    poll
   end
 end
