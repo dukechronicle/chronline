198d4931bd3aa2c44b59d45763475374a6a5304f
Michael Lai <themichaellai@gmail.com>
2014-01-24 00:31:57 -0500

    Added new/edit poll form

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,7 +1,9 @@
 class Poll < ActiveRecord::Base
-  attr_accessible :description, :title, :section
+  attr_accessible :description, :title, :section, :choice_ids
 
   has_many :choices, class_name: 'Poll::Choice', dependent: :destroy
+
+  accepts_nested_attributes_for :choices
 
   validates :title, presence: true
 end
--- app/models/poll/choice.rb
+++ app/models/poll/choice.rb
@@ -4,4 +4,24 @@ class Poll::Choice < ActiveRecord::Base
   belongs_to :poll
 
   validates :title, presence: true
+
+  default_value_for(:votes) { 0 }
+
+  def self.find_create_or_delete_poll_choices(poll, titles)
+    unused_choices = poll.choices.reject { |choice| not titles.include? choice.title }
+    unused_choices.each { |choice| choice.destroy }
+
+    found_choices = {}
+    poll.choices.where(title: titles).each do |choice|
+      found_choices[choice.title] = choice
+    end
+    titles.each do |title|
+      self.transaction do
+        if not found_choices.has_key?(title)
+          found_choices[title] = Poll::Choice.create!(title: title)
+        end
+      end
+    end
+    found_choices.values
+  end
 end
