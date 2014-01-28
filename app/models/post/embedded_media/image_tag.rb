51270769f5842c1fbd85863fdc1962c976b2f8fa
Austin Miller Kyker <amk66@duke.edu>
2014-01-28 15:20:50 -0500

    Embedded images pop out with lightbox2

--- app/models/post/embedded_media/image_tag.rb
+++ app/models/post/embedded_media/image_tag.rb
@@ -15,7 +15,7 @@ class Post
         classes = "embedded-image embedded-#{float}"
         content_tag(:span, nil, class: classes) do
           photo_credit = photo_credit(@image, link: true)
-          concat content_tag(:img, nil, **image_attributes)
+          concat content_tag(:a, content_tag(:img, nil,**image_attributes), 'data-lightbox' => "image-1", href: @image.original.url(@style))
           concat content_tag(:span, photo_credit, class: 'photo-credit')
         end
       end
