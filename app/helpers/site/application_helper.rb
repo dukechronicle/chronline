7942b75a1710880d00e530d1c318238b19447454
Michael Lai <themichaellai@gmail.com>
2014-02-03 17:14:24 -0500

    Fix ad auid

--- app/helpers/site/application_helper.rb
+++ app/helpers/site/application_helper.rb
@@ -14,7 +14,7 @@ module Site::ApplicationHelper
 <noscript>
   <iframe id="#{unit_id}" name="#{unit_id}" src="//ox-d.oncampusweb.com/w/1.0/afr?auid=#{@@types[zone]['unit']}&cb=#{Random.rand(99999999)}" frameBorder="0" frameSpacing="0" scrolling="no" width="#{@@types[zone]['width']}" height="#{@@types[zone]['height']}">
     <a href="//ox-d.oncampusweb.com/w/1.0/rc?cs=#{unit_id}&cb=#{Random.rand(99999999)}">
-    <img src="//ox-d.oncampusweb.com/w/1.0/ai?auid=536871739&cs=#{unit_id}&cb=#{Random.rand(99999999)}" border="0" alt="">
+    <img src="//ox-d.oncampusweb.com/w/1.0/ai?auid=#{@@types[zone]['unit']}&cs=#{unit_id}&cb=#{Random.rand(99999999)}" border="0" alt="">
     </a>
   </iframe>
 </noscript>
