360ef2cbdad3f0a3a40c2925f181cde2a4d6a288
Glenn Rivkees <glenn.rivkees@gmail.com>
2014-02-03 03:03:30 -0500

    begin article page

--- app/controllers/beta/articles_controller.rb
+++ app/controllers/beta/articles_controller.rb
@@ -1,5 +1,35 @@
 class Beta::ArticlesController < Beta::BaseController
+  include ::PostsController
+
+  before_filter :redirect_article, only: [:show, :print]
+
+
   def index
+    begin
+      @taxonomy = Taxonomy.new(:sections, "/#{params[:section]}/")
+    rescue Taxonomy::InvalidTaxonomyError
+      return not_found
+    end
+
+    begin
+      custom_page and return
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
+
+    @articles = Article
+      .includes(:authors, :image)
+      .section(@taxonomy)
+      .order('published_at DESC')
+      .page(params[:page])
+    unless @taxonomy.root?
+      @popular = Article.popular(@taxonomy[0].downcase, limit: 5)
+    end
+  end
+
+  def show
+    @article.register_view
+    @taxonomy = @article.section
   end
 
 end
