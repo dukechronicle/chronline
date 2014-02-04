545213883f65eee5ae4d756a5d940c3d83025898
Michael Lai <themichaellai@gmail.com>
2014-02-03 22:57:24 -0500

    Move ad tag html to partial

--- app/helpers/site/advertisement_helper.rb
+++ app/helpers/site/advertisement_helper.rb
@@ -9,19 +9,15 @@ module Site::AdvertisementHelper
     zone = suffix ? "#{zone}_#{suffix}" : zone.to_s
     page_id = '536870985'
     a_random = Random.rand(99999999)
-    <<EOS
-<script type="text/javascript">
-    OX_#{GROUP_ID}.showAdUnit(#{AD_UNITS[zone]['unit']});
-</script>
-<noscript>
-  <iframe id="#{GROUP_ID}" name="#{GROUP_ID}" src="//ox-d.oncampusweb.com/w/1.0/afr?auid=#{AD_UNITS[zone]['unit']}&cb=#{a_random}" frameBorder="0" frameSpacing="0" scrolling="no" width="#{AD_UNITS[zone]['width']}" height="#{AD_UNITS[zone]['height']}">
-    <a href="//ox-d.oncampusweb.com/w/1.0/rc?cs=#{GROUP_ID}&cb=#{a_random}">
-    <img src="//ox-d.oncampusweb.com/w/1.0/ai?auid=#{AD_UNITS[zone]['unit']}&cs=#{GROUP_ID}&cb=#{a_random}" border="0" alt="">
-    </a>
-  </iframe>
-</noscript>
-EOS
-      .html_safe
+    render(
+      partial: 'site/partials/advertisement_tag', layout: false, locals: {
+        group_id: GROUP_ID,
+        unit: AD_UNITS[zone]['unit'],
+        width: AD_UNITS[zone]['width'],
+        height: AD_UNITS[zone]['height'],
+        a_random: a_random,
+      }
+    ).html_safe
   end
 
   def ad_group_id
