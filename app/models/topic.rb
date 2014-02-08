4dbb6e3b8692401cdd3f84845c759a41534960d4
Roger <rogerzou94@gmail.com>
2014-02-08 16:27:58 -0500

    fix env variables, change routes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,10 +1,10 @@
 class Topic < ActiveRecord::Base
   
-	has_many :responses, class_name: 'Topic::Response', dependent: :destroy
-	
-	attr_accessible :title, :description, :archived
+  has_many :responses, class_name: 'Topic::Response', dependent: :destroy
+  
+  attr_accessible :title, :description, :archived
 
-	validates :title, presence: true
-	validates :description, presence: true, length: { maximum: 100 }
+  validates :title, presence: true
+  validates :description, presence: true, length: { maximum: 100 }
 
 end
