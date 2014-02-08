d79005d4bc83e94bd93b188b5322e770a122d391
Roger <rogerzou94@gmail.com>
2014-02-08 16:14:48 -0500

    add jim's comments

--- app/controllers/admin/topic_responses_controller.rb
+++ app/controllers/admin/topic_responses_controller.rb
@@ -1,30 +1,30 @@
 class Admin::TopicResponsesController < Admin::BaseController
 
-	def create
-		@response = Topic.find(params[:topic_id]).responses.build(params[:topic_response])
-		@response.approved = true
-		if @response.save
-			# some message?
-		end
-		redirect_to :back
-	end
+  def create
+    @response = Topic.find(params[:topic_id]).responses.build(params[:topic_response])
+    @response.approved = true
+    if @response.save
+      # some message?
+    end
+    redirect_to :back
+  end
 
-	def report
-		@response = Topic::Response.find(params[:id])
-		@response.update_attributes(reported: !@response.reported)
-		redirect_to :back
-	end
+  def report
+    @response = Topic::Response.find(params[:id])
+    @response.update_attributes(reported: !@response.reported)
+    redirect_to :back
+  end
 
-	def approve
-		@response = Topic::Response.find(params[:id])
-		@response.update_attributes(approved: !@response.approved)
-		redirect_to :back
-	end
+  def approve
+    @response = Topic::Response.find(params[:id])
+    @response.update_attributes(approved: !@response.approved)
+    redirect_to :back
+  end
 
-	def destroy
-		@response = Topic::Response.find(params[:id])
-		@response.destroy
-		redirect_to :back
-	end
+  def destroy
+    @response = Topic::Response.find(params[:id])
+    @response.destroy
+    redirect_to :back
+  end
 
 end
--- app/models/topic/response.rb
+++ app/models/topic/response.rb
@@ -1,38 +1,38 @@
 class Topic::Response < ActiveRecord::Base
 
-	@@blacklist = File.read("config/blacklist.txt").split(",").collect(&:strip).to_set
+  @@blacklist = File.read("config/blacklist.txt").split(",").collect(&:strip).to_set
 
  # HAX: needed so that url_for works correctly for blog posts
   self.model_name.instance_variable_set(:@singular_route_key, "response")
   self.model_name.instance_variable_set(:@route_key, "responses")
 
-	belongs_to :topic
+  belongs_to :topic
 
-	attr_accessible :content, :approved, :reported, :upvotes, :downvotes
+  attr_accessible :content, :approved, :reported, :upvotes, :downvotes
 
-	validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 140 }
 
-	before_create :blacklist
+  before_save :blacklist
 
-	# checks content for blacklisted words. If there is a blacklisted word,
-	# the response is automatically reported.
-	# TODO include subsets of words. So, 'bitches' is blacklisted even though 
-	# only 'bitch' is in the blacklist. Is this a good method? May not be efficient
-	def blacklist
-		content = self.content.split(/\W+/)
-		content.each do |word|
+  # checks content for blacklisted words. If there is a blacklisted word,
+  # the response is automatically reported.
+  # TODO include subsets of words. So, 'bitches' is blacklisted even though 
+  # only 'bitch' is in the blacklist. Is this a good method? May not be efficient
+  def blacklist
+    content = self.content.split(/\W+/)
+    content.each do |word|
 
-			@@blacklist.each do |bword|
-				if bword.include?(word)
-					self.reported = true
-				end
-			end
+      @@blacklist.each do |bword|
+        if bword.include?(word)
+          self.reported = true
+        end
+      end
 =begin
-			if @@blacklist.include?(word)
-				self.reported = true
-			end
+      if @@blacklist.include?(word)
+        self.reported = true
+      end
 =end
-		end
-	end
+    end
+  end
 
 end
