430729663084fb05e5227580ab10b3c4d96b3216
Michael Lai <themichaellai@gmail.com>
2014-02-07 22:45:28 -0500

    Save vote to session

--- app/controllers/site/polls_controller.rb
+++ app/controllers/site/polls_controller.rb
@@ -1,14 +1,33 @@
 class Site::PollsController < Site::BaseController
   def show
     @poll = Poll.find(params[:id])
-    @choices = @poll.choices.order('votes DESC, title ASC')
+    if session_poll_vote(@poll)
+      @choices = @poll.choices.order('votes DESC, title ASC')
+    else
+      @choices = @poll.choices.order('title ASC')
+    end
+    @voted = not(session_poll_vote(@poll).nil?)
   end
 
   def vote
-    @poll = Poll.find(params[:id])
     @choice = Poll::Choice.find(params[:choice])
-    @choice.update_attributes(votes: @choice.votes + 1)
-    redirect_to site_poll_path(@poll)
+    vote_for(@choice)
+    redirect_to site_poll_path(@choice.poll)
+  end
+
+  private
+  def session_poll_vote(poll)
+    session["poll#{poll.id}"]
+  end
+
+  def set_session_poll_vote(choice)
+    session["poll#{choice.poll.id}"] = choice.id
+  end
+
+  def vote_for(choice)
+    if not session_poll_vote(choice.poll)
+      set_session_poll_vote(choice)
+      choice.update_attributes(votes: choice.votes + 1)
+    end
   end
 end
-
