372f1b747e1620b08e5f743fc4d8b98eb4c88ab7
Jim Posen <jim.posen@gmail.com>
2014-02-11 11:54:35 -0500

    Admin show action for users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,19 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    if params[:email].present?
+      user = User.find_by_email(params[:email])
+      redirect_to [:admin, user] and return
+    end
     @users = User
       .order(:email)
       .page(params[:page])
     if params[:role].present?
+      @role = params[:role]
       @users = @users.where(role: params[:role])
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
