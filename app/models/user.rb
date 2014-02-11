a2f1e3be8c67637ebe20608941ea82fcff7f333a
Jim Posen <jim.posen@gmail.com>
2014-02-11 17:12:39 -0500

    Refactored OAuth controller and model code.

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,17 +26,8 @@ class User < ActiveRecord::Base
   end
 
   class << self
-    def find_for_facebook_oauth(auth)
-      self.where(facebook_uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
-        user.first_name = auth.info.first_name
-        user.last_name = auth.info.last_name
-      end
-    end
-
-    def find_for_google_oauth(auth)
-      self.where(google_uid: auth.uid).first_or_create do |user|
+    def find_for_oauth(provider, auth)
+      self.where("#{provider}_uid" => auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
         user.first_name = auth.info.first_name
