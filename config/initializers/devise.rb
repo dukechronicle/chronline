df9e9071934f974231a74cc94fd56c5f0058b5d2
Jim Posen <jim.posen@gmail.com>
2014-02-10 21:57:39 -0500

    Devise mailer sender name is more legitimate.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = ENV['GMAIL_USERNAME']
+  config.mailer_sender = "#{ENV['SMTP_FROM_NAME']} <#{ENV['SMTP_FROM_EMAIL']}>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
