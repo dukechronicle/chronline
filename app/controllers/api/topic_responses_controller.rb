d04e92b040554017b9b3598c7e26f9f77579e888
Jim Posen <jim.posen@gmail.com>
2014-02-14 12:23:35 -0500

    Force valid CSRF token for topic responses controller.

--- app/controllers/api/topic_responses_controller.rb
+++ app/controllers/api/topic_responses_controller.rb
@@ -1,14 +1,13 @@
 class Api::TopicResponsesController < Api::BaseController
-
   REPORT_LIMIT = 10
 
   def index
     @topic = Topic.find(params[:topic_id])
     if @topic
       @responses = @topic.responses
-      .where("reported = false OR approved = true")
-      .order('created_at DESC')
-      .paginate(page: params[:page], per_page: 30)
+        .where("reported = false OR approved = true")
+        .order('created_at DESC')
+        .paginate(page: params[:page], per_page: 30)
       respond_with_topic_responses @responses, status: :ok
     else
       respond_with status: :unprocessable_entity
@@ -70,71 +69,74 @@ class Api::TopicResponsesController < Api::BaseController
     respond_with status: :ok
   end
 
-  private
+  def handle_unverified_request
+    head :forbidden and return
+  end
 
-    def report_helper
-      if session[:reports].nil?
-        session[:reports] = 1
-      else
-        session[:reports] = session[:reports] + 1
-      end
-      if session[:reports] > REPORT_LIMIT
-        return false
-      end
+  private
+  def report_helper
+    if session[:reports].nil?
+      session[:reports] = 1
+    else
+      session[:reports] = session[:reports] + 1
+    end
+    if session[:reports] > REPORT_LIMIT
+      return false
+    end
+    return true
+  end
+
+  def session_upvote_status(response_id)
+    return session_status_helper(response_id, :upvotes)
+  end
+
+  def session_downvote_status(response_id)
+    return session_status_helper(response_id, :downvotes)
+  end
+
+  # false means has not voted
+  def session_status_helper(response_id, up_or_down)
+    votestatus = votes_hashparam(response_id, up_or_down)
+    if session[votestatus].nil? or not session[votestatus]
+      session[votestatus] = true
+      return false
+    else
+      session[votestatus] = false
       return true
     end
+  end
 
-    def session_upvote_status(response_id)
-      return session_status_helper(response_id, :upvotes)
-    end
+  def votes_hashparam(response_id, option)
+    (option.to_s + "_" + response_id.to_s).to_sym
+  end
 
-    def session_downvote_status(response_id)
-      return session_status_helper(response_id, :downvotes)
-    end
-
-    # false means has not voted
-    def session_status_helper(response_id, up_or_down)
-      votestatus = votes_hashparam(response_id, up_or_down)
-      if session[votestatus].nil? or not session[votestatus]
-        session[votestatus] = true
-        return false
-      else
-        session[votestatus] = false
-        return true
-      end
-    end
-
-    def votes_hashparam(response_id, option)
-      (option.to_s + "_" + response_id.to_s).to_sym
-    end
-
-    # for multiple responses
-    def respond_with_topic_responses(responses, options = {})
-      options.merge!(
-        properties: {
-          upvoted: ->(response) {
-            session[votes_hashparam(response.id, :upvotes)] ||= false
-          },
-          downvoted: ->(response) {
-            session[votes_hashparam(response.id, :downvotes)] ||= false
-          },
-          reports: ->(response) {
-            session[:reports] ||= 0
-          },
-        }
-      )
-      respond_with responses, options
-    end
-
-    # for a single Topic::Response
-    def respond_with_topic_response(response, options = {})
-      response_hash = response.as_json
-      params = {
-            upvoted: session[votes_hashparam(response.id, :upvotes)] ||= false,
-            downvoted: session[votes_hashparam(response.id, :downvotes)] ||= false,
-            reports: session[:reports] ||= 0
+  # for multiple responses
+  def respond_with_topic_responses(responses, options = {})
+    options.merge!(
+      properties: {
+        upvoted: ->(response) {
+          session[votes_hashparam(response.id, :upvotes)] ||= false
+        },
+        downvoted: ->(response) {
+          session[votes_hashparam(response.id, :downvotes)] ||= false
+        },
+        reports: ->(response) {
+          session[:reports] ||= 0
+        },
       }
-      response_hash.merge!(params)
-      render json: response_hash, status: options[:status]
-    end
+    )
+    respond_with responses, options
+  end
+
+  # for a single Topic::Response
+  def respond_with_topic_response(response, options = {})
+    response_hash = response.as_json
+    params = {
+      upvoted: session[votes_hashparam(response.id, :upvotes)] ||= false,
+      downvoted: session[votes_hashparam(response.id, :downvotes)] ||= false,
+      reports: session[:reports] ||= 0
+    }
+    response_hash.merge!(params)
+    render json: response_hash, status: options[:status]
+  end
 end
