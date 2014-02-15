d1fba97a11fa33ada7a74a9bd5d7a0ba6b7da912
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-02-15 18:08:38 -0500

    gallery slugs working with redirect for social shares

--- app/controllers/site/galleries_controller.rb
+++ app/controllers/site/galleries_controller.rb
@@ -1,5 +1,6 @@
 class Site::GalleriesController < Site::BaseController
 
+  before_filter :redirect_gallery, only: :show
   def index
     @galleries =  Gallery.order('date DESC')
     begin
@@ -12,6 +13,20 @@ class Site::GalleriesController < Site::BaseController
   def show
     @gallery = Gallery.find(params[:id])
     @recent = Gallery.order('created_at DESC').limit(3).reject(&:empty?)
+  end
+
+  private
+  def redirect_gallery
+    @gallery = Gallery.find(params[:id])
+    expected_path = url_for(
+      id: @gallery,
+      controller: controller_path,
+      action: action_name,
+      only_path: true
+    )
+    if not social_crawler? and request.path != expected_path
+      return redirect_to expected_path, status: :moved_permanently
+    end
   end
 
 end
