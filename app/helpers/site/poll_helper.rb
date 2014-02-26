b6ee30ce176101eb2bf93de26f2b03102581f0df
Michael Lai <themichaellai@gmail.com>
2014-02-26 11:55:27 -0500

    Readded twitter box for sports, fixed choice editing

--- app/helpers/site/poll_helper.rb
+++ app/helpers/site/poll_helper.rb
@@ -2,4 +2,8 @@ module Site::PollHelper
   def voted_on_poll?(poll)
     not session["poll#{poll.id}"].nil?
   end
+
+  def poll_json(poll)
+    poll.to_json(include: :choices, properties: {voted: ->(poll) { voted_on_poll?(poll) }})
+  end
 end
