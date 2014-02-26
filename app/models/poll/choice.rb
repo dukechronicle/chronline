05bd935d04750602add6ac8197aec8bb9a5c243e
Michael Lai <themichaellai@gmail.com>
2014-02-26 11:55:27 -0500

    Readded twitter box for sports, fixed choice editing

--- app/models/poll/choice.rb
+++ app/models/poll/choice.rb
@@ -10,7 +10,7 @@ class Poll::Choice < ActiveRecord::Base
   default_scope order: 'votes DESC, title ASC'
 
   def self.find_create_or_delete_poll_choices(poll, titles)
-    unused_choices = poll.choices.reject { |choice| not titles.include? choice.title }
+    unused_choices = poll.choices.reject { |choice| titles.include? choice.title }
     unused_choices.each(&:destroy)
 
     found_choices = {}
