4437f0567b6c5888ff030bbe16e8a5164dd6394c
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-02-26 22:44:37 -0500

    moved functionality out of rake task, added flash message, made put a post

--- app/workers/photoshelter_worker.rb
+++ app/workers/photoshelter_worker.rb
@@ -1,10 +1,6 @@
-require 'rake'
-Rake::Task.clear # necessary to avoid tasks being loaded several times in dev mode
-Chronline::Application.load_tasks 
 class PhotoshelterWorker
   @queue = :photoshelter
   def self.perform
-    Rake::Task['photoshelter:scrape'].reenable
-    Rake::Task['photoshelter:scrape'].invoke
+    Gallery.scrape
   end
 end
