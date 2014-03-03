d6fa6b7f8b87cd3de63037ec97a8c9b6b27fcdf0
Prithvi Prabahar <prithvi923@gmail.com>
2014-03-03 00:02:11 -0500

    Fix Photoshelter API tests

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -37,7 +37,8 @@ class Gallery < ActiveRecord::Base
   end
 
   def self.scrape
-    PhotoshelterAPI.instance.get_all_galleries.each do |gallery|
+    @api = PhotoshelterAPI.new
+    @api.get_all_galleries.each do |gallery|
       if !Gallery.exists?(:gid => gallery['id']) then
         begin
           date = Date.strptime gallery['name'], '%Y/%m/%d'
@@ -46,10 +47,10 @@ class Gallery < ActiveRecord::Base
         end
         gallery_name = gallery['name'].gsub(/[0-9]+\/[0-9]+\/[0-9]+/, '').sub(/^[\s\-]+/, '')
         Gallery.create(gid: gallery['id'], name: gallery_name, description: gallery['description'], date: date)
-        images = PhotoshelterAPI.instance.get_gallery_images gallery['id']
+        images = @api.get_gallery_images gallery['id']
         if images then
           images.each do |image|
-            info = PhotoshelterAPI.instance.get_image_info image['id']
+            info = @api.get_image_info image['id']
             if !Gallery::Image.exists?(:pid => image['id'], :gid => gallery['id']) then
               Gallery::Image.create(:pid => image['id'], :gid => gallery['id'], :caption => info['caption'], :credit => info['credit'], :title => info['title'])
             end
--- spec/lib/photoshelter_api_spec.rb
+++ spec/lib/photoshelter_api_spec.rb
@@ -4,15 +4,13 @@ require 'spec_helper'
 
 describe PhotoshelterAPI do
 
-  subject { PhotoshelterAPI.new(Settings.photoshelter.email, Settings.photoshelter.password) }
-
-  describe "#authenticate" do
+  describe "authenticate" do
 
     context "when authenticated with an invalid email" do
-      use_vcr_cassette "bad_username"
+      use_vcr_cassette 'bad_username'
 
-      it "should raise PhotoshelterError" do
-        constructor = -> { PhotoshelterAPI.new("fail", Settings.photoshelter.password).authenticate }
+      it "raises PhotoshelterError" do
+        constructor = -> { PhotoshelterAPI.new.authenticate }
         constructor.should raise_error(PhotoshelterAPI::PhotoshelterError)
       end
     end
@@ -20,63 +18,67 @@ describe PhotoshelterAPI do
     context "when authenticated with an invalid password" do
       use_vcr_cassette "bad_password"
 
-      it "should raise PhotoshelterError" do
-        constructor = -> { PhotoshelterAPI.new(Settings.photoshelter.email, "fail").authenticate }
+      it "raises PhotoshelterError" do
+        constructor = -> { PhotoshelterAPI.new.authenticate }
         constructor.should raise_error(PhotoshelterAPI::PhotoshelterError)
       end
     end
 
-    describe "when authenticated with correct credentials" do
-      use_vcr_cassette "authentication"
+    context "when authenticated with correct credentials" do
+      use_vcr_cassette 'authentication'
 
-      it "should authenticate properly" do
-        subject.authenticate.should == true
+      it "authenticates properly" do
+        @response = PhotoshelterAPI.new.authenticate
+        @response.should == true
+      end
+    end
+  end
+
+  describe "authenticated routes" do
+
+    subject(:photoshelterapi) do
+      PhotoshelterAPI.new
+    end
+
+    describe "GET galleries" do
+      use_vcr_cassette 'galleries'
+
+      it "returns list of galleries" do
+        @response = photoshelterapi.get_all_galleries
+        @response.should be_a Array
       end
     end
 
-  end
+    describe "GET gallery images" do
+      use_vcr_cassette 'images'
 
-  describe "GET galleries" do
-    use_vcr_cassette "galleries"
-
-    it "should return list of galleries" do
-      subject.authenticate
-      subject.get_all_galleries.should be_a Array
+      it "returns a list of images for a gallery" do
+        @response = photoshelterapi.get_gallery_images "G0000W3g4oBchl8c"
+        @response.should be_a Array
+      end
     end
-  end
 
-  describe "GET gallery images" do
-    use_vcr_cassette "images"
+    describe "GET image info" do
+      use_vcr_cassette 'info'
 
-    it "should return a list of images for a gallery" do
-      subject.authenticate
-      images = subject.get_gallery_images "G0000W3g4oBchl8c"
-      images.should be_a Array
+      it "returns information for an image" do
+        @response = photoshelterapi.get_image_info "I0000P1V4eGUln18"
+        @response.should be_a Hash
+
+        keys = @response.keys
+        keys.should include "title"
+        keys.should include "author"
+        keys.should include "caption"
+      end
     end
-  end
 
-  describe "GET image info" do
-    use_vcr_cassette "info"
+    describe "GET logout" do
+      use_vcr_cassette 'logout'
 
-    it "should return information for an image" do
-      subject.authenticate
-      info = subject.get_image_info "I0000P1V4eGUln18"
-      info.should be_a Hash
-
-      keys = info.keys
-      keys.should include "title"
-      keys.should include "author"
-      keys.should include "caption"
-    end
-  end
-
-  describe "GET logout" do
-    use_vcr_cassette "logout"
-
-    it "should logout and destroy the session" do
-      subject.authenticate
-      subject.logout
-      expect { subject.get_image_info}.to raise_error
+      it "logs out and destroys the session" do
+        @response = photoshelterapi.logout
+        expect { photoshelterapi.get_image_info}.to raise_error
+      end
     end
   end
 end
