56ec8681ee2753bb0f11cf894b8cf6e4d7bf7335
Jim Posen <jim.posen@gmail.com>
2014-03-16 16:31:46 -0400

    Game preview modal pops up on double click.

--- app/controllers/site/tournaments_controller.rb
+++ app/controllers/site/tournaments_controller.rb
@@ -6,8 +6,13 @@ class Site::TournamentsController < Site::BaseController
       @bracket = @tournament.brackets.find_by_user_id(current_user)
     end
     @games = @tournament.games
-      .includes(:team1, :team2)
+      .includes(team1: :article, team2: :article)
       .order(:position)
-      .to_json(include: [:team1, :team2])
+      .to_json(
+        include: {
+          team1: { include: :article },
+          team2: { include: :article }
+        }
+      )
   end
 end
