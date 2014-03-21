adc81122a0aa1a34b2015818d30f3b406e526564
Jim Posen <jim.posen@gmail.com>
2014-03-21 14:58:06 -0400

    All embedded media are figures. May be either full width or floated.

--- app/models/post/embedded_media/tag.rb
+++ app/models/post/embedded_media/tag.rb
@@ -6,9 +6,32 @@ class Post
         require_dependency file
       end
 
+      def figure_html(float)
+        figure_classes = [
+          "embedded-#{float || 'full'}",
+          "embedded-#{self.class.tag_name.underscore.dasherize}"
+        ]
+        content_tag :figure, nil, class: figure_classes do
+          concat to_html
+          concat content_tag(:figcaption, caption) if caption
+        end
+      end
+
+      def caption
+        nil
+      end
+
+      def full_width?
+        true
+      end
+
+      def self.tag_name
+        self.name.demodulize.sub(/Tag$/, '')
+      end
+
       def self.parse_tag(tag, embedded_media)
         match = /{{([a-zA-Z]*):([^\}]*?)}}/.match(tag)
-        if match.present? and match[1].eql? self.name.demodulize.sub(/Tag$/, '')
+        if match.present? and match[1].eql? tag_name
           self.new(embedded_media, *match[2].split(','))
         else
           nil
