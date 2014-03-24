f047f40ca89fba03b7eb8da899469dd1c942f292
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-03-23 21:11:53 -0400

    moved locale settings to separate script

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-# Setup locales
-sudo touch /etc/profile.d/lang.sh
-sudo chmod 777 /etc/profile.d/lang.sh
-sudo echo 'export LANGUAGE="en_US.UTF-8"' >> /etc/profile.d/lang.sh
-sudo echo 'export LANG="en_US.UTF-8"' >> /etc/profile.d/lang.sh
-sudo echo 'export LC_ALL="en_US.UTF-8"' >> /etc/profile.d/lang.sh
-
-# Prepare locales
-locale-gen en_US.UTF-8
-sudo dpkg-reconfigure locales
-
 # Update package manager
 sudo apt-get -y update
 
