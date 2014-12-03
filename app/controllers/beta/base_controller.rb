3f7e3b4cefd43301a1272495cc2c572cc8f36160
Michael Lai <themichaellai@gmail.com>
2014-12-02 22:55:18 -0500

    Added sitemap proxy

--- app/controllers/beta/base_controller.rb
+++ app/controllers/beta/base_controller.rb
@@ -13,4 +13,9 @@ class Beta::BaseController < ApplicationController
     render 'beta/404', status: :not_found
   end
 
+  def sitemap_proxy
+    res = HTTParty.get("http://#{ENV['AWS_S3_BUCKET']}.s3.amazonaws.com" +
+                       "/sitemaps/news/sitemap1.xml.gz")
+    send_data res.body
+  end
 end
