a0d9c4e21cb3bbd8eb1dff500ee5f7fc40593f06
Tyler Nisonoff <tylernisonoff@gmail.com>
2014-12-22 16:15:15 -0500

    finished sponsor page

---
+++ app/models/page/layouts/sponsor.rb
@@ -0,0 +1,35 @@
+class Page::Layouts::Sponsor < Layout
+  def schema
+    {
+      'logo_url' => {
+        'label' => 'Logo URL',
+        'required' => true,
+        'type' => 'string',
+      },
+      'description' => {
+        'label' => 'Description',
+        'required' => true,
+        'extends' => markdown_schema,
+      },
+      'sponsors' => {
+        'label' => 'Sponsors',
+        'type' => 'array',
+        'items' => {
+          'type' => 'object',
+          'properties' => {
+            'image_url' => {
+              'label' => 'image url',
+              'type' => 'string',
+              'required' => true
+            },
+            'image_link'  => {
+              'label' => 'image link',
+              'type' => 'string',
+              'required' => true
+            }
+          }
+        }
+      }
+    }
+  end
+end
