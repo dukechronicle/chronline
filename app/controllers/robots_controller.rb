04fb4380016799973ae8fc1dbe8f1459a2ec3f22
gautamh <gautamhathi@live.com>
2015-01-18 13:14:55 -0500

    changed robots controller to update robots.txt dynamically based on database

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,12 +1,13 @@
 class RobotsController < ApplicationController
 
   def show
-    file = case request.subdomain.to_sym
-           when :m   then :mobile
-           when :www then :site
-           else request.subdomain
-           end
-    render "#{file}.txt"
+    output = "User-agent: *\n"
+    Article.where(block_bots: true).find_each do |article|
+      output = output+ "Disallow: /#{article.slug}\n"
+      output = output+ "Disallow: /#{article.slug}/print\n"
+    end
+
+    render text: output, :content_type => "text/plain"
   end
 
 end
