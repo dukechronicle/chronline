b503591a611a8ba9443f2e2f8b5d2adf98b5a962
Michael Lai <themichaellai@gmail.com>
2015-02-28 20:14:14 -0500

    Display polls on sidebar of articles

--- app/helpers/site/poll_helper.rb
+++ app/helpers/site/poll_helper.rb
@@ -2,4 +2,9 @@ module Site::PollHelper
   def voted_on_poll?(poll)
     not session["poll#{poll.id}"].nil?
   end
+
+  def find_section_poll(taxonomy)
+    hierarchy = taxonomy.parents
+    Poll.where(section: hierarchy.map(&:to_s)).order("LENGTH(section) DESC").first
+  end
 end
